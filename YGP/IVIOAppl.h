06d9147dd26daf7695d9c07e9772885df98f1bc9
Markus <Markus>
1999-08-11 17:22:45 +0000

    Constructor sets long-options

--- YGP/IVIOAppl.h
+++ YGP/IVIOAppl.h
@@ -1,7 +1,7 @@
 #ifndef IVIOAPPL_H
 #define IVIOAPPL_H
 
-//$Id: IVIOAppl.h,v 1.3 1999/08/11 16:19:54 Markus Exp $
+//$Id: IVIOAppl.h,v 1.4 1999/08/11 17:22:45 Markus Exp $
 
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Library General Public
@@ -17,21 +17,24 @@
 // License along with this library; if not, write to the Free
 // Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+
 // Class to handle the startup of a program
 class IVIOApplication {
- public:
-   // Manager functions
-   IVIOApplication (const int argc, char* argv[]);
-   virtual ~IVIOApplication () { }
-
-   int run ();
-
- protected:
+   // Helper-structure to store long-options
    typedef struct {
       char* longVal;
       char  shortVal;
    } longOptions;
 
+ public:
+   // Manager functions
+   IVIOApplication (const int argc, char* argv[],
+		    const longOptions* pOpt = NULL);
+   virtual ~IVIOApplication () { }
+
+   int run ();
+
+ protected:
    // Option-handling
    virtual bool handleOption (const char option) = 0;
    char*        getOptionValue ();
