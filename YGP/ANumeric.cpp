e76c95950318a707fb996633e98f527d0c2a1e77
Markus <Markus>
1999-09-11 00:40:40 +0000

    Comparison and calculation added

--- YGP/ANumeric.cpp
+++ YGP/ANumeric.cpp
@@ -1,11 +1,11 @@
-//$Id: ANumeric.cpp,v 1.2 1999/09/05 01:33:54 Markus Rel $
+//$Id: ANumeric.cpp,v 1.3 1999/09/11 00:40:40 Markus Rel $
 
 //PROJECT     : General
 //SUBSYSTEM   : ANumeric
 //REFERENCES  :
 //TODO        :
 //BUGS        :
-//REVISION    : $Revision: 1.2 $
+//REVISION    : $Revision: 1.3 $
 //AUTHOR      : Markus Schwab
 //CREATED     : 22.7.1999
 //COPYRIGHT   : Anticopyright (A) 1999
@@ -25,6 +25,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
+#include <assert.h>
 #include <locale.h>
 
 #include <iomanip>
@@ -52,6 +53,7 @@ struct lconv* loc (localeconv ());
 //Purpose   : Destructor
 /*--------------------------------------------------------------------------*/
 ANumeric::~ANumeric () {
+   mpz_clear (value);
 }
 
 /*--------------------------------------------------------------------------*/
@@ -61,7 +63,7 @@ ANumeric::~ANumeric () {
 /*--------------------------------------------------------------------------*/
 ANumeric& ANumeric::operator= (const ANumeric& other) {
    if (this != &other) {
-      value = other.value;
+      mpz_set (value, other.value);
       AttributValue::operator= ((const AttributValue&) other);
    }
    return *this;
@@ -71,15 +73,165 @@ ANumeric& ANumeric::operator= (const ANumeric& other) {
 //Purpose   : Converting to a string
 //Returns   : String-representation of ANumeric
 /*--------------------------------------------------------------------------*/
-ANumeric::operator std::string () const {
-   char buffer[20] = "";
+std::string ANumeric::toString () const {
+   std::string str ("");
+
    if (isDefined ()) {
-      ostrstream temp (buffer, sizeof (buffer));
-      print (temp, value);
+      char* pString (mpz_get_str (NULL, 10, value)); assert (pString);
+      register char* pHelp (pString); assert (pString);
+
+      if (*pHelp == '-')
+         str = *pHelp++;
+
+      register int len (strlen (pHelp) % 3);    // Copy first bytes til 3-byte-
+      str.replace (str.length (), 0, pHelp, len);  // boundary; split with sep.
+      pHelp += len;
+      len = strlen (loc->thousands_sep);
+      while (*pHelp) {                                // Copy til end-of-string
+         str.replace (str.length (), 0, loc->thousands_sep, len);
+
+	 assert (pHelp[1]); assert (pHelp[2]);       // pHelp should be aligned
+         str.replace (str.length (), 0, pHelp, 3);        // to 3-byte-boundary
+         pHelp += 3;
+     } // end-while
+
+      free (pString);
    }
-   return buffer;
+   return str;
 }
 
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adds a value to this
+//Parameters: lhs: Value to add
+//Returns   : Self
+//Note      : If lhs is not defined this is not changed
+/*--------------------------------------------------------------------------*/
+ANumeric& ANumeric::operator+= (const ANumeric& lhs) {
+   if (lhs.isDefined ()) {
+      mpz_add (value, value, lhs.value);
+      AttributValue::define ();
+   }
+   return *this;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Substract a value from this
+//Parameters: lhs: Value to substract
+//Returns   : Self
+//Note      : If lhs is not defined this is not changed
+/*--------------------------------------------------------------------------*/
+ANumeric& ANumeric::operator-= (const ANumeric& lhs) {
+   if (lhs.isDefined ()) {
+      mpz_sub (value, value, lhs.value);
+      AttributValue::define ();
+   }
+   return *this;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Multiply this with a value
+//Parameters: lhs: Value to multiply
+//Returns   : Self
+//Note      : If lhs is not defined this is not changed; if this is not defined
+//            it is set to lhs
+/*--------------------------------------------------------------------------*/
+ANumeric& ANumeric::operator*= (const ANumeric& lhs) {
+   if (lhs.isDefined ())
+      if (isDefined ())
+         mpz_mul (value, value, lhs.value);
+      else
+         operator= (lhs);
+   return *this;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Divides this through a value
+//Parameters: lhs: Value to divide with
+//Returns   : Self
+//Note      : If lhs is not defined this is not changed; if this is not defined
+//            it is set to lhs
+/*--------------------------------------------------------------------------*/
+ANumeric& ANumeric::operator/= (const ANumeric& lhs) {
+   if (lhs.isDefined ())
+      if (isDefined ())
+         mpz_tdiv_q (value, value, lhs.value);
+      else
+         operator= (lhs);
+   return *this;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Compares two ANumeric-values
+//Parameters: other: Object to compare
+//Returns   : >0 if this > other; 0 if this == other; <0 else
+//Note      : Undefined values are considered as between 0 and -1
+/*--------------------------------------------------------------------------*/
+int ANumeric::compare (const ANumeric& other) {
+   if (isDefined () && other.isDefined ())     // Both sides defined -> compare
+      return mpz_cmp (value, other.value);
+
+   int rc;
+   if (isDefined ())                            // this defined: Compare with 0
+      rc = mpz_cmp_ui (value, 0);              // other defined: Compare with 0
+   else if (other.isDefined ())                      // Both not defined: Equal
+      return 0;
+   else
+      rc = -mpz_cmp_ui (other.value, 0);
+   return rc ? rc : 1;                // If comp with 0 is equal: Return bigger
+}
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adds two ANumeric-values
+//Parameters: lhs: Left-hand-side of addition
+//            rhs: Right-hand-side of addition
+//Returns   : ANumeric: Result of additon
+//Note      : Undefined values are ignored
+/*--------------------------------------------------------------------------*/
+ANumeric operator+ (const ANumeric& lhs, const ANumeric& rhs) {
+   ANumeric result (lhs);
+   result += rhs;
+   return result;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Substracts two ANumeric-values
+//Parameters: lhs: Left-hand-side of substraction
+//            rhs: Right-hand-side of substraction
+//Returns   : ANumeric: Result of substraction
+//Note      : Undefined values are ignored
+/*--------------------------------------------------------------------------*/
+ANumeric operator- (const ANumeric& lhs, const ANumeric& rhs) {
+   ANumeric result (lhs);
+   result -= rhs;
+   return result;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Multiplies two ANumeric-values
+//Parameters: lhs: Left-hand-side of mulitplication
+//            rhs: Right-hand-side of mulitplication
+//Returns   : ANumeric: Result of mulitplication
+//Note      : Undefined values are ignored
+/*--------------------------------------------------------------------------*/
+ANumeric operator* (const ANumeric& lhs, const ANumeric& rhs) {
+   ANumeric result (lhs);
+   result *= rhs;
+   return result;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Divides two ANumeric-values
+//Parameters: lhs: Left-hand-side of division
+//            rhs: Right-hand-side of division
+//Returns   : ANumeric: Result of division
+//Note      : Undefined values are ignored
+/*--------------------------------------------------------------------------*/
+ANumeric operator/ (const ANumeric& lhs, const ANumeric& rhs) {
+   ANumeric result (lhs);
+   result /= rhs;
+   return result;
+}
 
 /*--------------------------------------------------------------------------*/
 //Purpose   : Output of ANumeric
@@ -88,25 +240,7 @@ ANumeric::operator std::string () const {
 //Returns   : Reference to out
 /*--------------------------------------------------------------------------*/
 ostream& operator<< (ostream& out, const ANumeric& outValue) {
-   if (outValue.isDefined ()) {
-      std::string temp (outValue);
-      out << temp;
-   }
+   if (outValue.isDefined ())
+      out << outValue.toString ();
    return out;
 }
-
-/*--------------------------------------------------------------------------*/
-//Purpose   : Helper-function for formatted printing of a long-value
-//Parameter : out: Out-stream
-//            outValue: long to print
-/*--------------------------------------------------------------------------*/
-void print (ostream& out, long outValue) {
-   if (outValue >= 1000) {
-      print (out, outValue / 1000);
-      assert (loc);
-      out << loc->thousands_sep << setw (3) << setfill ('0');
-      outValue %= 1000;
-   }
-   out << outValue;
-}
-
--- YGP/ANumeric.h
+++ YGP/ANumeric.h
@@ -1,7 +1,7 @@
 #ifndef ANUMERIC_H
 #define ANUMERIC_H
 
-//$Id: ANumeric.h,v 1.2 1999/09/05 01:33:54 Markus Rel $
+//$Id: ANumeric.h,v 1.3 1999/09/11 00:40:40 Markus Rel $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,33 +18,56 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
+#include <gmp.h>
+
+#include <string>
+
 #include "AttrVal.h"
 
 
-// Class for numeric attributes
-// TODO: Calculation-methods
+// Class for numeric attributes. As every AttributValue is supports undefined
+// values. Furthermore the range is unlimitted (thanks to GNUs Multi Precission
+// library)
 class ANumeric : public AttributValue {
  public:
-   ANumeric () : AttributValue (), value (0) { }
-   ANumeric (const ANumeric& other)
-     : AttributValue ((const AttributValue&)other) {
-     value = other.value; }
-   ANumeric (const long val) : AttributValue (), value (val) {
+   ANumeric () : AttributValue () { mpz_init_set_ui (value, 0); }
+   ANumeric (const ANumeric& other) : AttributValue ((const AttributValue&)other) {
+     mpz_init_set (value, other.value); }
+   ANumeric (const long val) : AttributValue () {
+      mpz_init_set_si (value, val);
       AttributValue::define (); }
    virtual ~ANumeric ();
 
    ANumeric& operator= (const ANumeric& other);
-   ANumeric& operator= (long val) { define (); value = val; }
+   ANumeric& operator= (long val) { AttributValue::define ();  mpz_set_si (value, val); }
 
-   virtual void define () { AttributValue::define (); value = 0; }
+   virtual void define () { AttributValue::define (); mpz_set (value, 0); }
+   virtual std::string toString () const;
 
-   operator long () const { return value; }
-   operator std::string () const;
+   // Calculation
+   ANumeric& operator += (const ANumeric& lhs);
+   ANumeric& operator -= (const ANumeric& lhs);
+   ANumeric& operator *= (const ANumeric& lhs);
+   ANumeric& operator /= (const ANumeric& lhs);
+
+   friend ANumeric operator+ (const ANumeric& lhs, const ANumeric& rhs);
+   friend ANumeric operator- (const ANumeric& lhs, const ANumeric& rhs);
+   friend ANumeric operator* (const ANumeric& lhs, const ANumeric& rhs);
+   friend ANumeric operator/ (const ANumeric& lhs, const ANumeric& rhs);
+
+   // Comparison
+   bool operator== (const ANumeric& other) { return !compare (other); }
+   bool operator!= (const ANumeric& other) { return compare (other); }
+   bool operator<  (const ANumeric& other) { return compare (other) < 0; }
+   bool operator>  (const ANumeric& other) { return compare (other) > 0; }
+   bool operator<= (const ANumeric& other) { return compare (other) <= 0; }
+   bool operator>= (const ANumeric& other) { return compare (other) >= 0; }
+   int compare (const ANumeric& other);
 
    friend ostream& operator<< (ostream& out, const ANumeric& outValue);
 
  private: 
-   long value;
+   mpz_t value;
 };
 
 #endif
