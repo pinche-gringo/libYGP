a811c34480d8d23dee767f4dc5d47882702dd760
Markus <Markus>
1999-10-12 00:22:04 +0000

    Initial revision of date-class

---
+++ YGP/ADate.cpp
@@ -0,0 +1,320 @@
+//$Id: ADate.cpp,v 1.1 1999/10/12 00:22:04 Markus Rel $
+
+//PROJECT     : General
+//SUBSYSTEM   : ADate
+//REFERENCES  :
+//TODO        :
+//BUGS        :
+//REVISION    : $Revision: 1.1 $
+//AUTHOR      : Markus Schwab
+//CREATED     : 11.10.1999
+//COPYRIGHT   : Anticopyright (A) 1999
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#include <time.h>
+#include <values.h>
+#include <locale.h>
+
+#include <iomanip>
+#include <iostream.h>
+#ifdef UNIX
+#include <strstream.h>
+#else
+#include <strstrea.h>
+#endif
+
+#define DEBUG 0
+#include "Trace.h"
+
+#include "ADate.h"
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Constructor
+//Parameters: now: Flag if current time or default start-time (1.1.1900)
+//                 should be set
+/*--------------------------------------------------------------------------*/
+ADate::ADate (bool now) : AttributValue () {
+   define ();
+   if (now) {
+      time_t curtime (time (NULL));
+      struct tm* locTime (localtime (&curtime)); assert (locTime);
+      day = locTime->tm_mday;
+      month = locTime->tm_mon + 1;                   // tm_mon is from 0 to 11
+      year = locTime->tm_year + 1900;
+   }
+   else {
+      day = month = 1;
+      year = 1900;
+   }
+   assert (!checkIntegrity ());
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Constructor
+//Parameters: day: Day for this ADate
+//            month: Month
+//            year: Year to set
+/*--------------------------------------------------------------------------*/
+ADate::ADate (char Day, char Month, unsigned int Year) : AttributValue () {
+   day = Day;
+   month = Month;
+   year = Year; assert (checkIntegrity ());
+
+   maxAdapt ();
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Destructor
+/*--------------------------------------------------------------------------*/
+ADate::~ADate () {
+}
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Assignment-operator
+//Parameters: other: Object to copy
+//Returns   : Reference to self
+/*--------------------------------------------------------------------------*/
+ADate& ADate::operator= (const ADate& other) {
+   if (this != &other) {
+      TRACE5 ("ADate::operator=: " << other);
+
+      day = other.day;
+      month = other.month;
+      year = other.year; assert (!checkIntegrity ());
+      AttributValue::operator= ((const AttributValue&) other);
+
+      assert (!checkIntegrity ());
+   }
+   return *this;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Converting to a string
+//Returns   : String-representation of ADate
+/*--------------------------------------------------------------------------*/
+std::string ADate::toString () const {
+   return toString ("%x");
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Converting to a string giving a certain format (acc .to strftime);
+//Returns   : String-representation of ADate
+/*--------------------------------------------------------------------------*/
+std::string ADate::toString (const char* format) const {
+   assert (format);
+
+   char szBuffer[20] = "";
+
+   if (isDefined ()) {
+      struct tm tm = { 0, 0, 0, day, month - 1, year - 1900 };
+      strftime (szBuffer, sizeof (szBuffer), format, &tm);
+   }
+   return szBuffer;
+}
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adds a value to this
+//Parameters: lhs: Value to add
+//Returns   : Self
+//Note      : If lhs is not defined this is not changed
+/*--------------------------------------------------------------------------*/
+ADate& ADate::operator+= (const ADate& rhs) {
+   if (rhs.isDefined ()) {
+      day += rhs.day;
+      month += rhs.month;
+      year += rhs.year;
+      
+      maxAdapt ();
+      AttributValue::define ();
+   }
+   return *this;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Substract a value from this
+//Parameters: lhs: Value to substract
+//Returns   : Self
+//Note      : If lhs is not defined this is not changed
+/*--------------------------------------------------------------------------*/
+ADate& ADate::operator-= (const ADate& rhs) {
+   if (rhs.isDefined ()) {
+      day -= rhs.day;
+      month -= rhs.month;
+      year -= rhs.year;
+
+      minAdapt ();
+      AttributValue::define ();
+   }
+   return *this;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adds a value to this
+//Parameters: day: Day to add
+//            month: Month to add
+//            year: Year to add
+//Returns   : Self
+/*--------------------------------------------------------------------------*/
+ADate& ADate::add (char Day, char Month, unsigned int Year) {
+   day += Day;
+   month += Month;
+   year += Year;
+   maxAdapt ();
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adds a value to this
+//Parameters: day: Day to substract
+//            month: Month to substract
+//            year: Year to substract
+//Returns   : Self
+/*--------------------------------------------------------------------------*/
+ADate& ADate::sub (char Day, char Month, unsigned int Year) {
+   day -= Day;
+   month -= Month;
+   year -= Year;
+   minAdapt ();
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Compares two ADate-values
+//Parameters: other: Object to compare
+//Returns   : >0 if this  other; 0 if this == other; <0 else
+//Note      : Undefined values are considered as (incredible) old
+//            "Younger dates" (closer to the past) are considered bigger
+//            than "older dates" (further in the past; that means the numeric
+//            value of the date is compared.
+/*--------------------------------------------------------------------------*/
+long ADate::compare (const ADate& other) {
+   assert (!checkIntegrity ()); assert (!other.checkIntegrity ());
+
+   // Both sides are defined -> return (approximated) difference
+   if (isDefined () && other.isDefined ()) {
+     TRACE5 ("ADate::compare -> " << ((year - other.year) * 365
+                                      + ((month - other.month) * 31)
+                                      + day - other.day));
+
+      return ((year - other.year) * 365 + ((month - other.month) * 31)
+              + day - other.day);
+   }
+
+   if (isDefined ())                             // this defined: Return bigger
+      return MAXLONG;
+   else if (other.isDefined ())                // other defined: Return smaller
+      return MINLONG;
+   else
+      return 0;                               // Both not defined: Return equal
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adds two ADate-values
+//Parameters: lhs: Left-hand-side of addition
+//            rhs: Right-hand-side of addition
+//Returns   : ADate: Result of additon
+//Note      : Undefined values are ignored
+/*--------------------------------------------------------------------------*/
+ADate operator+ (const ADate& lhs, const ADate& rhs) {
+   ADate result (lhs);
+   result += rhs;
+   return result;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Substracts two ADate-values
+//Parameters: lhs: Left-hand-side of substraction
+//            rhs: Right-hand-side of substraction
+//Returns   : ADate: Result of substraction
+//Note      : Undefined values are ignored
+/*--------------------------------------------------------------------------*/
+ADate operator- (const ADate& lhs, const ADate& rhs) {
+   ADate result (lhs);
+   result -= rhs;
+   return result;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Checks the status of the object
+//Returns   : Status; 0: OK
+/*--------------------------------------------------------------------------*/
+int ADate::checkIntegrity () const {
+   return ((month < 1) || (month > 12)) ? 2 : (day > maxDayOf ());
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Checks the maximal day of the passed month/year-combination
+//Parameters: month: Month to check
+//            year: Year to check
+//Returns   : int: Max. day
+/*--------------------------------------------------------------------------*/
+char  ADate::maxDayOf (char month, unsigned int year) {
+  if (month == 2)                              // Special-handling of february
+     return isLeapYear (year) ? 29 : 28;
+
+  if (month > 7)              // Adapt month after july for easier calculation
+     --month;
+  return month & 1 ? 31 : 30;
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Checks if the passed day is a leap-year (years which can be
+//            divided by 4; except if it is also divideable by 100)
+//Parameters  year: Year to check
+//Returns   : bool: True, if leap-year
+/*--------------------------------------------------------------------------*/
+bool ADate::isLeapYear (unsigned int year) {
+   TRACE9 ("ADate::isLeapYear: " << year << " %4 = " << (year & 3) << " %100 = "
+           << (year % 100) << " %400 = " << (year % 400));
+   return (year & 3) ? false : (year % 100) ? true : !(year % 400);
+}
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adapt value after recalculation with possible underflow
+/*--------------------------------------------------------------------------*/
+void ADate::minAdapt () {
+   if (day < 1)                                     // Adapt date if underflow
+      --month;
+
+   if ((month < 1) || (month > 12)) {              // Adapt month if underflow
+      month += 12;              // Assuming calculation was with correct month
+      --year;
+   }
+
+   if (day < 1)                  // Finish day-adaption after month-correction
+      day += maxDayOf ();
+
+   assert (!checkIntegrity ());
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Adapt value after recalculation with possible overflow
+/*--------------------------------------------------------------------------*/
+void ADate::maxAdapt () {
+   int maxDay (maxDayOf ());                         // Adapt date if overflow
+   if (day > maxDay) {
+      day -= maxDay;
+      ++month;
+   }
+
+   if (month > 12) {                                // Adapt month if overflow
+      month -= 12;              // Assuming calculation was with correct month
+      ++year;
+   }
+   assert (!checkIntegrity ());
+}
---
+++ YGP/ADate.h
@@ -0,0 +1,91 @@
+#ifndef ADATE_H
+#define ADATE_H
+
+//$Id: ADate.h,v 1.1 1999/10/12 00:22:04 Markus Rel $
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <time.h>
+#include <assert.h>
+
+#include <string>
+
+#include "AttrVal.h"
+
+
+// Class for date attributes. As every AttributValue is supports undefined
+// values.
+class ADate : public AttributValue {
+ public:
+   ADate () : AttributValue () { }
+   ADate (bool now);
+   ADate (const ADate& other) : AttributValue ((const AttributValue&)other)
+      , day (other.day), month (other.month), year (other.year) { }
+   ADate (char Day, char Month, unsigned int Year);
+   virtual ~ADate ();
+
+   ADate& operator= (const ADate& other);
+
+   void setDay (char Day) { assert ((Day > 0) && (Day <= maxDayOf ())); day = Day; }
+   void setMonth (char Month) { assert ((Month > 0) && (Month < 12)); month = Month; }
+   void setYear (unsigned int Year) { year = Year; }
+
+   char setDay () { return day; }
+   char setMonth () { return month; }
+   unsigned int setYear () { return year; }
+
+   static ADate ADate::today () { return ADate (true); }
+
+   virtual void define () { AttributValue::define (); day = month = 1; year = 1900; }
+   virtual std::string toString () const;
+   virtual std::string toString (const char* format) const;
+
+   // Calculation
+   ADate& operator+= (const ADate& rhs);
+   ADate& operator-= (const ADate& rhs);
+
+   friend ADate operator+ (const ADate& lhs, const ADate& rhs);
+   friend ADate operator- (const ADate& lhs, const ADate& rhs);
+
+   ADate& add (char Day, char Month = 0, unsigned int Year = 0);
+   ADate& sub (char Day, char month = 0, unsigned int Year = 0);
+
+   // Comparison
+   bool operator== (const ADate& other) { return !compare (other); }
+   bool operator!= (const ADate& other) { return compare (other); }
+   bool operator<  (const ADate& other) { return compare (other) < 0; }
+   bool operator>  (const ADate& other) { return compare (other) > 0; }
+   bool operator<= (const ADate& other) { return compare (other) <= 0; }
+   bool operator>= (const ADate& other) { return compare (other) >= 0; }
+   long compare (const ADate& other);
+
+   char maxDayOf () const { return maxDayOf (month, year); }
+   static char maxDayOf (char month, unsigned int year);
+   bool isLeapYear () const { return isLeapYear (year); }
+   static bool isLeapYear (unsigned int year);
+
+   int checkIntegrity () const;
+
+ private: 
+   char day;
+   char month;
+   unsigned int year;
+
+   void minAdapt ();
+   void maxAdapt ();
+};
+
+#endif
