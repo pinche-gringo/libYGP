310f1cb71dfbf03a11d5f841b4fb8ae7c28c8bb4
Markus <Markus>
2000-07-17 10:55:38 +0000

    Usability with GCC added (default)

--- Make.bat
+++ Make.bat
@@ -1,3 +1,20 @@
+Rem @Echo Off
+If "%1" == "MSC" Goto MSC
+If "%1" == "GCC" Goto GCC
+Goto Default
+
+:GCC
+Shift
+:Default
+cd Common
+Call Make -f ..\Windows\GCCTest.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
+Goto End
+
+:MSC
+Shift
 cd Windows
-nmake /f Test.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
+Call NMake /f ..\Windows\Test.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
+Goto End
+
+:End
 cd ..
