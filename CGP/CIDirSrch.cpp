e2ee9ea60ab4d8c27324bad7aeaf39986dc21a3c
Markus <Markus>
2001-01-24 16:27:05 +0000

    Class-declaration moved into header

--- CGP/CIDirSrch.cpp
+++ CGP/CIDirSrch.cpp
@@ -1,14 +1,14 @@
- //$Id: CIDirSrch.cpp,v 1.1 2001/01/07 00:48:46 Markus Exp $
+//$Id: CIDirSrch.cpp,v 1.2 2001/01/24 16:27:05 Markus Exp $
 
 //PROJECT     : General/CORBA
 //SUBSYSTEM   : CDirSrch
 //REFERENCES  :
 //TODO        :
 //BUGS        :
-//REVISION    : $Revision: 1.1 $
+//REVISION    : $Revision: 1.2 $
 //AUTHOR      : Markus Schwab
-//CREATED     : 7.1.2000
-//COPYRIGHT   : Anticopyright (A) 2000
+//CREATED     : 7.1.2001
+//COPYRIGHT   : Anticopyright (A) 2001
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -25,45 +25,64 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
-#include <CORBA.h>
-
 #include "ATStamp.h"
-#include "DirSrch.h"
-#include "CDirSrch.h"
-
-class CIDirEntry : virtual public CDirEntry_skel, virtual public dirEntry {
- public:
-   char* path () { return const_cast <char*> (dirEntry::path ()); }
-   char* name () { return const_cast <char*> (dirEntry::name ()); }
-   CORBA::ULong size () { return dirEntry::size (); }
-   char* time () {
-      ATimestamp time (dirEntry::time ());
-      return const_cast<char*> (time.toString ().c_str ());
-   }
-   CORBA::ULong attributes () { return dirEntry::attributes (); }
-
-   CORBA::Boolean isDirectory () { return dirEntry::isDirectory (); }
-   CORBA::Boolean isExecuteable () { return dirEntry::isExecuteable (); }
-   CORBA::Boolean isUserExec () { return dirEntry::isUserExec (); }
-
-   CORBA::Short compare (const char* file) {
-      return dirEntry::compare (file); }
-   CORBA::Short compareObject (CDirEntry_ptr other) {
-      return dirEntry::compare (dynamic_cast<dirEntry*> (other)); }
-};
+#include "CIDirSrch.h"
 
 
-class CIDirectorySearch : virtual public CDirectorySearch,
-			  virtual public DirectorySearch {
- public:
-   CDirEntry_ptr find (const char* file, CORBA::ULong attr) {
-      return DirectorySearch::find (file, entry, attr) ? &entry : NULL; }
-   CDirEntry_ptr findnext () { return DirectorySearch::find () ? &entry : NULL; }
+CIDirEntry::CIDirEntry (const dirEntry& other) : dirEntry (other) {
+}
 
-   char* getFile () { return const_cast<char*> (DirectorySearch::getFile ().c_str ()); }
+char* CIDirEntry::path () {
+   return const_cast <char*> (dirEntry::path ());
+}
 
-   CORBA::Char getSplitChar () { return DirectorySearch::getSplitChar (); }
+char* CIDirEntry::name () {
+   return const_cast <char*> (dirEntry::name ());
+}
 
- private:
-   CIDirEntry entry;
-};
+CORBA::ULong CIDirEntry::size () {
+   return dirEntry::size ();
+}
+
+char* CIDirEntry::time () {
+   ATimestamp time (dirEntry::time ());
+   return const_cast<char*> (time.toString ().c_str ());
+}
+
+CORBA::ULong CIDirEntry::attributes () {
+   return dirEntry::attributes ();
+}
+
+CORBA::Boolean CIDirEntry::isDirectory () {
+   return dirEntry::isDirectory ();
+}
+
+CORBA::Boolean CIDirEntry::isExecuteable () {
+   return dirEntry::isExecuteable ();
+}
+
+CORBA::Boolean CIDirEntry::isUserExec () {
+   return dirEntry::isUserExec ();
+}
+
+CORBA::Short CIDirEntry::compare (const char* file) {
+   return dirEntry::compare (file);
+}
+
+CORBA::Short CIDirEntry::compareObject (CDirEntry_ptr other) {
+   return dirEntry::compare (dynamic_cast<dirEntry*> (other));
+}
+
+
+
+CDirEntry_ptr CIDirectorySearch::find (const char* file, CORBA::ULong attr) {
+   return DirectorySearch::find (file, entry, attr) ? &entry : NULL;
+}
+
+CDirEntry_ptr CIDirectorySearch::findnext () {
+   return DirectorySearch::find () ? &entry : NULL;
+}
+
+char* CIDirectorySearch::getFile () {
+   return const_cast<char*> (DirectorySearch::getFile ().c_str ());
+}
