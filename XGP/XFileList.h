3b68d8d46baa7dbefb4e7b9c3ec258fa9e6d8e23
Markus Schwab <g17m0@lycos.com>
2002-04-11 07:59:35 +0000

    Description improved

--- XGP/XFileList.h
+++ XGP/XFileList.h
@@ -1,7 +1,7 @@
 #ifndef XFILELIST_H
 #define XFILELIST_H
 
-//$Id: XFileList.h,v 1.9 2002/04/09 04:10:52 markus Exp $
+//$Id: XFileList.h,v 1.10 2002/04/11 07:59:35 markus Rel $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -37,9 +37,15 @@ struct File;
 
 // Class for a (columned) list which holds files represented by an icon
 // and textual information.
-// The icons of the files are loaded by default from all Icon.*-files in the
-// package-data-directory; user-specified can be added to or overridden from
-// this list.
+// The icons of the files are loaded by default from all Icon_*.xpm-files in
+// the package-data-directory; user-specified ones can be added to or overridden
+// from this list.
+// The user (or whoever) can easily add his own icons by adding files (named
+// according to this rule) by adding xpm-files into that directory.
+// E.g: To add a special icon for netscape: Put a(n) (xpm-)file called
+// Icon_netscape.xpm into /usr/local/share/General (replace /usr/local with
+// the prefix where you installed the library into).
+// Note: Only the name is inspected to determine the icon; not the path!
 class XFileList : public CList {
  public:
    XFileList (int columns, const gchar *titles[] = 0)
