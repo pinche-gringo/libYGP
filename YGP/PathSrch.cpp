fb5212a58cc78be69b84a2a19f6923685566308a
Markus Schwab <g17m0@lycos.com>
2002-05-09 06:57:59 +0000

    Initial revision

---
+++ YGP/PathSrch.cpp
@@ -0,0 +1,107 @@
+//$Id: PathSrch.cpp,v 1.1 2002/05/09 06:57:59 markus Rel $
+
+//PROJECT     : General
+//SUBSYSTEM   : PathSrch
+//REFERENCES  :
+//TODO        :
+//BUGS        :
+//REVISION    : $Revision: 1.1 $
+//AUTHOR      : Markus Schwab
+//CREATED     : 9.5.2002
+//COPYRIGHT   : Anticopyright (A) 2002
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <pwd.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+#include "Trace_.h"
+
+#include "File.h"
+#include "PathSrch.h"
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Performs tilde-expansion on the input
+//            The expansion is only done under the following conditions
+//              - The string starts with a tilde
+//              - None of the characters up to the next whitespace or slash (/)
+//                is quoted
+//              - The following characters (if any) specify a user-name
+//            If those conditions apply, a single tilde is replaced with the
+//            contents of the HOME-environment variable (UNIX) or the combination
+//            of HOMEDRIVE and HOMEPATH (Windows). A tilde with following name
+//            returns the HOME-path of the user with that name (or the input, if
+//            such a user does not exist
+//Parameters: input: String to expand
+//Returns   : std::string: Expanded string or input (if not expandable)
+/*--------------------------------------------------------------------------*/
+std::string PathSearch::expandNode (const std::string& input) {
+   TRACE9 ("PathSearch::expandNode (const std::string&) - " << input);
+
+   if (input.empty () || input[0] != '~')
+      return input;
+
+   int i (1);
+   for (; i < input.length (); ++i) {
+      if (isspace (input[i]) || (input[i] == File::DIRSEPERATOR))
+         break;
+
+#if SYSTEM == UNIX
+      if (input[i] == '\\')                     // Don't expand quoted strings
+         return input;
+#endif
+   } // endfor
+
+   std::string ret (input);
+   if (i == 1) {                // No name after 
+#if SYSTEM == UNIX
+      const char* user = getenv ("HOME");
+      if (user)
+         ret.replace (0, 1, user);
+#else
+      const char* env = getenv ("HOMEDRIVE");
+      if (env) {
+         ret.replace (0, 1, env);
+         i = strlen (env);
+      }
+      else
+         ret.replace (0, 1, i = 0, '\0');
+
+      env = getenv ("HOMEPATH");
+      if (env)
+         ret.replace (i, 0, env);
+#endif
+   }
+   else {
+#if SYSTEM == UNIX
+      std::string user (input.substr (1, i));
+      TRACE5 ("PathSearch::expandNode (const std::string&) - Expanding user "
+              << user);
+
+      struct passwd* entry (getpwnam (user.c_str ()));
+      if (entry)
+         ret.replace (0, i, entry->pw_dir);
+
+#endif
+   }
+
+   TRACE1 ("PathSearch::expandNode (const std::string&) - Expanded user "
+           << ret);
+   return ret;
+}
