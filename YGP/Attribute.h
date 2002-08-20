0338311f6da6b49ab7bbbe32a08ebeabc1467879
Markus Schwab <g17m0@lycos.com>
2002-08-20 05:16:59 +0000

    Namespace added to used exception

--- YGP/Attribute.h
+++ YGP/Attribute.h
@@ -1,7 +1,7 @@
 #ifndef ATTRIBUTE_H
 #define ATTRIBUTE_H
 
-//$Id: Attribute.h,v 1.6 2002/05/24 06:41:32 markus Exp $
+//$Id: Attribute.h,v 1.7 2002/08/20 05:16:59 markus Exp $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -69,7 +69,7 @@ template <class T> class Attribute : public IAttribute {
          attr_ = value;
          return true;
       }
-      catch (invalid_argument&) {
+      catch (std::invalid_argument&) {
          return false;
       }
    }
