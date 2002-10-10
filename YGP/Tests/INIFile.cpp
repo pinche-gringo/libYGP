04d0417f5cde068ca20af9dc6b866793a865d5fc
Markus Schwab <g17m0@lycos.com>
2002-10-10 05:54:08 +0000

    Adapted to VC6.0; Fixed sequence of parsing

--- YGP/Tests/INIFile.cpp
+++ YGP/Tests/INIFile.cpp
@@ -1,11 +1,11 @@
-// $Id: INIFile.cpp,v 1.3 2002/05/01 00:43:29 markus Rel $
+// $Id: INIFile.cpp,v 1.4 2002/10/10 05:54:08 markus Exp $
 
 //PROJECT     : General
 //SUBSYSTEM   : Test/INIFile
 //REFERENCES  :
 //TODO        :
 //BUGS        :
-//REVISION    : $Revision: 1.3 $
+//REVISION    : $Revision: 1.4 $
 //AUTHOR      : Markus Schwab
 //CREATED     : 27.8.2001
 //COPYRIGHT   : Anticopyright (A) 2001
@@ -24,7 +24,9 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-#include <iostream.h>
+#include <gzo-cfg.h>
+
+#include <iostream>
 
 #include <ADate.h>
 #include <ATStamp.h>
@@ -35,20 +37,20 @@
 #include <Internal.h>
 #include "Test.h"
 
+#if SYSTEM == UNIX
+#  define PATH
+#else
+#  define PATH "..\\Common\\Tests\\"
+#endif
 
 int main (int argc, char* argv[]) {
-   setlocale (LC_ALL, "");
-   bindtextdomain (PACKAGE, LOCALEDIR);     // Specify messagefile for gettext
-   textdomain (PACKAGE);
-
    unsigned int cErrors (0);
 
-   cout << "Testing INI-file parser...\n";
+   std::cout << "Testing INI-file parser...\n";
    int Attr1;
    std::string Attr2;
    ANumeric Attr3;
    ADate Attr4;
-   Xifstream inifile;
    ATime Attr5;
    ATimestamp Attr6;
    try {
@@ -62,45 +64,38 @@ int main (int argc, char* argv[]) {
       global.addAttribute (attr3);
       global.addAttribute (attr4);
 
-      inifile.open ("INIFile.test", ios::in | ios::nocreate);
-      check (inifile);
+      INIFILE (PATH "INIFile.test");
+      INISECTION (Special);
+      INIATTR (Special, ATime, Attr5);
+      INIATTR (Special, ATimestamp, Attr6);
 
-      if (inifile) {
-         INIFILE ("INIFile.test");
-         INISECTION (Special);
-         INIATTR (Special, ATime, Attr5);
-         INIATTR (Special, ATimestamp, Attr6);
+      INISECTION (Local);
+      INIATTR (Local, int, Attr1);
+      std::string attr2_;
+      INIATTR2 (Local, std::string, attr2_, Attr2);
+      INIATTR (Local, ANumeric, Attr3);
+      INIATTR (Local, ADate, Attr4);
 
-         INISECTION (Local);
-         INIATTR (Local, int, Attr1);
-         std::string attr2_;
-         INIATTR2 (Local, std::string, attr2_, Attr2);
-         INIATTR (Local, ANumeric, Attr3);
-         INIATTR (Local, ADate, Attr4);
-
-         _inifile_.addSection (global);
-
-         inifile.init ();
-         try {
-            int rc = global.readFromStream ((Xistream&)inifile);
-            check (!rc);
-         } // end-try
-         catch (std::string& e) {
-            throw (std::string ("Error parsing INIFile.test in line ")
-                   + ANumeric (inifile.getLine ()).toString ()
-                   + std::string (" (") + ANumeric (inifile.getColumn ()).toString ()
-                   + std::string ("): ") + e);
-         } // end-catch
-
-         int rc = INIFILE_READ ();
+      try {
+         int rc = global.readFromStream ((Xistream&)_inifile_.getFile ());
          check (!rc);
-      } // endif
+      } // end-try
+      catch (std::string& e) {
+         throw (std::string ("Error parsing INIFile.test in line ")
+                + ANumeric (_inifile_.getFile ().getLine ()).toString ()
+                + std::string (" (")
+                + ANumeric (_inifile_.getFile ().getColumn ()).toString ()
+                + std::string ("): ") + e);
+      } // end-catch
+
+      int rc = INIFILE_READ ();
+      check (!rc);
    } // end-try
    catch (std::string& e) {
-      cerr << e.c_str () << '\n';
+      std::cerr << e.c_str () << '\n';
    }
 
    if (cErrors)
-      cout << "Failures: " << cErrors << '\n';
+      std::cout << "Failures: " << cErrors << '\n';
    return cErrors ? 1 : 0;
 }
