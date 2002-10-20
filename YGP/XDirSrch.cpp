a5fbf0dcac7fce6db1ab78ba218645d43261c476
Markus Schwab <g17m0@lycos.com>
2002-10-20 05:34:34 +0000

    Initial revision

---
+++ YGP/XDirSrch.cpp
@@ -0,0 +1,84 @@
+//$Id: XDirSrch.cpp,v 1.1 2002/10/20 05:34:34 markus Exp $
+
+//PROJECT     : General
+//SUBSYSTEM   : XDirectorySearch
+//REFERENCES  :
+//TODO        :
+//BUGS        :
+//REVISION    : $Revision: 1.1 $
+//AUTHOR      : Markus Schwab
+//CREATED     : 17.10.2002
+//COPYRIGHT   : Anticopyright (A) 2002
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include "Trace_.h"
+#include "FileRExp.h"
+#include "PathSrch.h"
+
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Checks the validity of the filename
+//Parameters: files: Nodes of files to in/exclude
+//            pFile: Pointer to filename
+//Returns   : bool: True: Name valid
+//Requires  : pFile is ASCIIZ-string
+//Remarks   : If the list starts with an x-node, a leading i*-node is assumed
+/*--------------------------------------------------------------------------*/
+bool _fileIsValid (const std::string& files, const char* pFile) {
+   TRACE9 ("_fileIsValid (const char*) const - " << pFile);
+   assert (pFile);
+
+   if (files.empty ())
+      return true;
+
+   PathSearch list (files);
+   string     node;
+
+   FileRegularExpr regexp (NULL);
+   bool include (false);
+
+   // Test every file in list
+   while (!((node = list.getNextNode ()).empty ())) {
+      TRACE8 ("_fileIsValid (const string&, const char*) const - Node: " << node);
+      assert ((node[0] == 'i') || (node[0] == 'x'));
+
+      include = node[0] == 'i';
+      node.replace (0, 1, 0, '\0');
+
+      regexp = node.c_str ();
+      if (regexp.matches (pFile))                      // Test if file matches
+          return include;
+   } // end-while
+
+   return !include;          // 'x'-nodes starts list -> Imply leading i*-node
+}
+
+/*--------------------------------------------------------------------------*/
+//Purpose   : Appends a node to a list. Every subnode in the node is prefixed
+//            with the prefix-character
+//Parameters: list: List to change
+//            prefix: Leading character
+//            node: Node to check
+//Notes     : The node is added to the beginning of the list
+/*--------------------------------------------------------------------------*/
+void _addNode (string& list, char prefix, const std::string& node) {
+   PathSearch  l (node);
+   std::string temp;
+
+   while (!(temp = l.getNextNode ()).empty ())
+      list = prefix + temp + (std::string)(PathSearch::PATHSEPARATOR + list);
+}
---
+++ YGP/XDirSrch.h
@@ -0,0 +1,67 @@
+#ifndef XDIRSRCH_H
+#define XDIRSRCH_H
+
+//$Id: XDirSrch.h,v 1.1 2002/10/20 05:34:34 markus Rel $
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <string>
+
+bool _fileIsValid (const std::string&, const char*);
+void _addNode (string& list, char prefix, const string& node);
+
+
+// Template to add file in/exlude to *DirectorySearch classes
+template <class Parent>
+class XDirectorySearch : public Parent {
+ public:
+   //@Section manager-functions
+   XDirectorySearch () : Parent () { }
+   XDirectorySearch (const string& spec) : Parent (spec) { }
+   virtual ~XDirectorySearch () { }
+
+   //@Section searching
+   virtual const File* find (const string& spec, unsigned long attribs = FILE_NORMAL) {
+      return Parent::find (spec, attribs); }
+   virtual const File* find (unsigned long attribs = FILE_NORMAL) {
+      const File* r = Parent::find (attribs);
+      return (r && !_fileIsValid (nodes, r->name ())) ? next () : r;
+   }
+   virtual const File* next () {
+      const File* r;
+      do {
+         r = Parent::next ();
+      } while (r && !_fileIsValid (nodes, r->name ()));
+      return r;
+   }
+
+   //@Section in/exclusion
+   void addFilesToInclude (const string& spec) { _addNode (nodes, 'i', spec); }
+   void addFilesToExclude (const string& spec) { _addNode (nodes, 'x', spec); }
+
+ private:
+   //@Section prohibited manager functions
+   XDirectorySearch (const IDirectorySearch&);
+   XDirectorySearch& operator= (const IDirectorySearch&);
+
+   std::string nodes;
+};
+
+
+typedef XDirectorySearch<DirectorySearch> XDirSrch;
+typedef XDirectorySearch<RemoteDirSearch> XRDirSrch;
+
+#endif
