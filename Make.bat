18574cf565842f8ba5a49d25ab59304e1a55358e
Markus Schwab <g17m0@lycos.com>
2002-11-04 00:40:20 +0000

    Make for GCC removed (use UNIX-style)

--- Make.bat
+++ Make.bat
@@ -1,20 +1,4 @@
 Rem @Echo Off
-If "%1" == "MSC" Goto MSC
-If "%1" == "GCC" Goto GCC
-Goto Default
-
-:GCC
-Shift
-:Default
-cd Common
-Call Make -f ..\Windows\GCCTest.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
-Goto End
-
-:MSC
-Shift
 cd Windows
 Call NMake /f VCGeneral.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
-Goto End
-
-:End
 cd ..
