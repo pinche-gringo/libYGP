bd68ed392ad70c8dbaf4ce8b83392de812ecab8a
Markus Schwab <g17m0@lycos.com>
2002-11-18 04:37:10 +0000

    Loading XAppl.so dynamically to show MsgBox also for VIO-appls

--- YGP/Check.cpp
+++ YGP/Check.cpp
@@ -1,11 +1,11 @@
-//$Id: Check.cpp,v 1.5 2002/11/13 04:30:02 markus Exp $
+//$Id: Check.cpp,v 1.6 2002/11/18 04:37:10 markus Exp $
 
 //PROJECT     : General
 //SUBSYSTEM   : Check
 //REFERENCES  :
 //TODO        :
 //BUGS        :
-//REVISION    : $Revision: 1.5 $
+//REVISION    : $Revision: 1.6 $
 //AUTHOR      : Markus Schwab
 //CREATED     : 13.9.1999
 //COPYRIGHT   : Anticopyright (A) 1999
@@ -30,20 +30,46 @@
 
 #include <iostream>
 
-#if SYSTEM == UNIX
+#include "Check.h"
 
+
+#if SYSTEM == UNIX
 #  ifdef HAVE_GTKMM
+#     include <dlfcn.h>
+#     include <gtk--/main.h>
 #     include <../X-windows/XMessageBox.h>
 
+      typedef int (*PFNCMSGBOX)(const char*, const char*, int, unsigned int);
+
       inline bool show (const char* expr, const char* title) {
-         return (XMessageBox::Show (expr, title, 
-                                    XMessageBox::CRITICAL | XMessageBox::OKCANCEL)
-                 != XMessageBox::OK); }
+         Main* appl (NULL);
+         if (!Main::instance ()) {
+            g_thread_init (NULL);
+            appl = new Main (0, NULL);
+         }
+
+         void* hDLL (dlopen ("libXAppl.so", 0x00001));
+         if (hDLL) {
+            PFNCMSGBOX pFnc ((PFNCMSGBOX)dlsym (hDLL, "showMessageBox"));
+            if (pFnc) {
+               gdk_threads_enter ();
+               bool rc (pFnc (expr, title,
+                              XMessageBox::CRITICAL | XMessageBox::OKCANCEL, 0)
+                        != XMessageBox::OK);
+               gdk_threads_leave ();
+               delete appl;
+               dlclose (hDLL);
+               return rc;
+            }
+         }
+         cerr << PACKAGE "-error: Couldn't display message box! " << dlerror () << '\n';
 #  else
       inline bool show (const char* expr, const char*) {
-         cerr << "Assertion '" << expr << "' failed\n";
-         return true; }
 #  endif
+         cerr << "Assertion failed! Continue y/n? ";
+         char ch;
+         cin >> ch;
+         return (ch != 'y') && (ch != 'Y'); }
 #elif SYSTEM == WINDOWS
 #  define WIN32_LEAN_AND_MEAN
 #  include <windows.h>
