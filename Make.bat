c8a5b12dd76398fc317c3bec9d2726aa68f1cba1
Markus Schwab <g17m0@lycos.com>
2002-12-17 20:39:14 +0000

    Making of DEF file moved to Makefile

--- Make.bat
+++ Make.bat
@@ -1,18 +1,4 @@
 Rem @Echo Off
 cd Windows
-If "%1" == "MakeDEF" Goto MakeDEF
 Call NMake /f VCGeneral.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
 cd ..
-Goto End
-
-:MakeDEF
-cd Debug
-Del ..\VCGeneral.def
-for %%i in (*.obj) do (dumpbin /SYMBOLS %%i | grep -E " External.*\(public:.+::[^ ]+\(.+\)\)")>>Temp.def
-Echo LIBRARY VCGeneral > ..\VCGeneral.def
-Echo EXPORTS >> ..\VCGeneral.def
-awk -f ..\..\bin\mdef.awk Temp.def>>..\VCGeneral.def
-Del Temp.def
-cd ..\..
-
-:End
