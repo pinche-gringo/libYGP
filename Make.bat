fe050f1be6b2db7e41d20fc96ce6ece47a749bbd
Markus Schwab <g17m0@lycos.com>
2003-02-14 04:17:22 +0000

    Handling BCC and MVC

--- Make.bat
+++ Make.bat
@@ -1,4 +1,15 @@
-Rem @Echo Off
+@Echo Off
+if "%1" == "MSC" (Set MAKE=NMake.exe&set MAKEFILE=/f VCGeneral.mak&shift&goto go)
+if "%1" == "BCC" (Set MAKE=Make.exe&set MAKEFILE=-f BCGeneral.mak&shift&goto go)
+
+echo Batch to compile the General DLL
+echo.
+echo Usage: %0 MSC^|BCC
+goto end
+
+:go
 cd Windows
-Call NMake /f VCGeneral.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
+Call %MAKE% %MAKEFILE% %1 %2 %3 %4 %5 %6 %7 %8 %9
 cd ..
+
+:end
