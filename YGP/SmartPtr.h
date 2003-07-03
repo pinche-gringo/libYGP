7a3547676663b64432051366876a409ce1d6c40e
Markus Schwab <g17m0@lycos.com>
2003-07-03 04:22:26 +0000

    Comments changed to doxygen style

--- YGP/SmartPtr.h
+++ YGP/SmartPtr.h
@@ -1,7 +1,7 @@
 #ifndef SMARTPTR_H
 #define SMARTPTR_H
 
-// $Id: SmartPtr.h,v 1.7 2002/12/01 21:19:32 markus Rel $
+// $Id: SmartPtr.h,v 1.8 2003/07/03 04:22:26 markus Rel $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,25 +18,48 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
-// Smart-pointer-class; its referenced object is freed when the smart
-// pointer-object is released.
-//
-// In counterpart to auto_ptr there is no ownership handling (and
-// therefor no copy-constructor nor an assignment-operator).
-// 
-// Note: This class is (intented to be) simple. Consider this when using it!
+/**Smart-pointer-class; its referenced object is freed when the smart
+   pointer-object is released.
+
+   In counterpart to auto_ptr there is no ownership handling (and
+   therefore no copy-constructor nor an assignment-operator).
+
+   The macro <tt>definePtr(class)</tt> can be used to create a type for
+   the smart pointer. It is defined as
+
+   \verbatim typedef SmartPtr<class> P##class;
+   \endverbatim
+
+   The macro <tt>defineConstPtr(class)</tt> is the const version of the macro
+   above:
+
+   \verbatim typedef SmartPtr<const class> PC##class;
+   \endverbatim
+
+   <tt>definePtr(string)</tt> would create a <tt>typedef SmartPtr<std::string>
+   Pstring</tt>.
+   
+   \note This class is (intented to be) simple. Consider this when using it!
+*/
 template <class T> class SmartPtr {
  public:
    // Manager functions
+   /// Constructor; creates a smart pointer managing the passed pointer
    SmartPtr (T* pValue = NULL) : pData (pValue) { }
+   /// Destructor; frees also the managed pointer
    ~SmartPtr () { delete pData; }
 
-   // Accessing
+   /// \name Accessing
+   //@{
+   /// Dereferencing the object
    T* operator-> () const { return pData; }
+   /// Dereferencing the object
    T& operator*  () const { return *pData; }
+   /// Casting to the underlaying type
    operator T*   () const { return pData; }
+   //@}
 
-   // Changing the value
+   /// Changing the managed pointer; the old managed object is freed
    SmartPtr& operator= (T* pValue) { delete pData; pData = pValue; return *this;}
 
  private:
