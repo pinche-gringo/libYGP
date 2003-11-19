1405b18cdbd354e461481476268b35eeb9a51709
Markus Schwab <g17m0@lycos.com>
2003-11-19 20:35:48 +0000

    Initial revision

---
+++ YGP/StatusObj.cpp
@@ -0,0 +1,106 @@
+//$Id: StatusObj.cpp,v 1.1 2003/11/19 20:35:48 markus Exp $
+
+//PROJECT     : Cardgames
+//SUBSYSTEM   : <FILLIN>
+//REFERENCES  :
+//TODO        :
+//BUGS        :
+//REVISION    : $Revision: 1.1 $
+//AUTHOR      : Markus Schwab
+//CREATED     : 19.11.2003
+//COPYRIGHT   : Anticopyright (A) 2003
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <YGP/StatusObj.h>
+
+
+namespace YGP {
+
+//-----------------------------------------------------------------------------
+/// Default constructor
+//-----------------------------------------------------------------------------
+StatusObject::StatusObject () : tp (INFO) {
+}
+
+//-----------------------------------------------------------------------------
+/// Constructor from a type and a message
+/// \param t: Type of the object
+/// \param message: Message of the object
+//-----------------------------------------------------------------------------
+StatusObject::StatusObject (type t, const std::string& message)
+    : tp (INFO), msg (message) {
+}
+
+//-----------------------------------------------------------------------------
+/// Copy constructor
+/// \param other: Object to copy
+//-----------------------------------------------------------------------------
+StatusObject::StatusObject (const StatusObject& other)
+    : tp (other.tp), msg (other.msg)
+      , child (other.child ? new StatusObject (*other.child) : NULL) {
+}
+
+//-----------------------------------------------------------------------------
+/// Destructor
+//-----------------------------------------------------------------------------
+StatusObject::~StatusObject () {
+}
+
+
+//-----------------------------------------------------------------------------
+/// Assignment operator
+/// \param other: Object to copy the values from
+//-----------------------------------------------------------------------------
+StatusObject& StatusObject::operator= (const StatusObject& other) {
+   if (&other != this) {
+      tp = other.tp;
+      msg = other.msg;
+      child = other.child ? new StatusObject (*other.child) : NULL;
+   }
+
+   return *this;
+}
+
+//----------------------------------------------------------------------------
+/// Sets a new status message (old messages are preserved)
+/// \param t: Type of the object
+/// \param message: Message of the object
+/// \remarks - The message is appended to existing ones
+///     - If the passed type is more important than the old one, it is changed 
+//----------------------------------------------------------------------------
+void StatusObject::setMessage (type t, const std::string& message) {
+   msg += message;
+   if (t > tp)
+      tp = t;
+}    
+
+//----------------------------------------------------------------------------
+/// Sets a new status message (old messages are preserved as "details")
+/// \param message: Message of the object
+/// \remarks - The message replaces existing ones
+///     - The old messages are accessable as "details"
+//----------------------------------------------------------------------------
+void StatusObject::abstract (const std::string& message) {
+    if (child)
+       child->msg += msg;
+    else
+       child = new StatusObject (tp, msg);
+
+    msg = message;
+}
+
+}
---
+++ YGP/StatusObj.h
@@ -0,0 +1,77 @@
+#ifndef STATUSOBJ_H
+#define STATUSOBJ_H
+
+//$Id: StatusObj.h,v 1.1 2003/11/19 20:35:48 markus Rel $
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#include <string>
+
+#include <YGP/SmartPtr.h>
+
+
+namespace YGP {
+
+/**Object holding status information.
+
+   This information can be extended by setting more information or abstracted
+   (setting a more general error text, while preserving the original
+   information as sort of details).
+*/
+class StatusObject {
+ public:
+   /// The type of the object
+   typedef enum { UNDEFINED,               ///< The object contains no message
+                  INFO,      ///< The object contains an informational message
+                  WARNING,          ///< The object contains a warning message
+                  ERROR } type;              ///< The object contains an error
+
+   StatusObject ();
+   StatusObject (type t, const std::string& message);
+   StatusObject (const StatusObject& other);
+   virtual ~StatusObject ();
+
+   StatusObject& operator= (const StatusObject& other);
+
+   /// Returns the type of the message
+   type getType () const { return tp; }
+
+   /// Returns the message
+   std::string getMessage () const { return msg; }
+
+   /// Returns the details of the message (after the object has been abstracted)
+   std::string getDetails () const { return child ? child->msg : ""; }
+
+
+   /// Returns if the message has details
+   bool hasDetails () const { return child; }
+
+   /// Cleans the object (sets it again to undefined)
+   void clean () { tp = UNDEFINED; msg.clear (); child = NULL; }
+
+   void setMessage (type t, const std::string& message);
+   void abstract (const std::string& message);
+
+ private:
+   type        tp;
+   std::string msg;
+
+   SmartPtr<StatusObject> child;
+};
+
+}
+
+#endif
