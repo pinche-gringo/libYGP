8d9b0962e081988274240b894800d9e9265b1590
Markus Schwab <g17m0@lycos.com>
2004-01-22 20:49:59 +0000

    Removed compilation for BCC

--- Make.bat
+++ Make.bat
@@ -1,15 +1,14 @@
 @Echo Off
-if "%1" == "MSC" (Set MAKE=NMake.exe&set MAKEFILE=/f VCGeneral.mak&shift&goto go)
-if "%1" == "BCC" (Set MAKE=Make.exe&set MAKEFILE=-f BCGeneral.mak&shift&goto go)
+if "%1" == "-h" goto help
 
-echo Batch to compile the General DLL
-echo.
-echo Usage: %0 MSC^|BCC
+cd Windows
+Call NMAKE /f VCGeneral.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
+cd ..
 goto end
 
-:go
-cd Windows
-Call %MAKE% %MAKEFILE% %1 %2 %3 %4 %5 %6 %7 %8 %9
-cd ..
+:help
+echo Batch to compile the General DLL
+echo.
+echo Usage: %0 [CFG=Release] [install^|check]
 
-:end
+:end
