9c22250e46a05eaa4a205a829919f72d3f92e49b
Markus Schwab <g17m0@lycos.com>
2004-11-28 01:04:26 +0000

    Added tests of casts

--- YGP/Tests/Handle.cpp
+++ YGP/Tests/Handle.cpp
@@ -1,11 +1,11 @@
-// $Id: Handle.cpp,v 1.7 2004/01/15 06:26:30 markus Rel $
+// $Id: Handle.cpp,v 1.8 2004/11/28 01:04:26 markus Rel $
 
 //PROJECT     : General
 //SUBSYSTEM   : Test/Handle
 //REFERENCES  :
 //TODO        :
 //BUGS        :
-//REVISION    : $Revision: 1.7 $
+//REVISION    : $Revision: 1.8 $
 //AUTHOR      : Markus Schwab
 //CREATED     : 27.8.2001
 //COPYRIGHT   : Copyright (C) 2001 - 2004
@@ -34,13 +34,21 @@
 
 class test {
  public:
-   const char* n;
    test (const char* name = "???") : n (name) {
-      TRACE1 ("Construct " << n);
-   }
-   ~test () {
-      TRACE1 ("Destruct " << n);
-   }
+      TRACE1 ("test::test (const char*) - " << n); }
+   ~test () { TRACE1 ("test::~test () - " << n); }
+
+   const char* n;
+};
+
+class test2 : public test {
+ public:
+   test2 (const char* name = "???") : test (name) {
+      TRACE1 ("test2::test2 (const char*) - " << n);
+      len = strlen (name); }
+   ~test2 () { TRACE1 ("test2::~test2 () - " << n); }
+
+   unsigned int len;
 };
 
 
@@ -49,15 +57,20 @@ int main (int argc, char* argv[]) {
 
    std::cout << "Testing SmartPtr & Handle...\n";
    definePtr (test);
-   Ptest pTest (new test ("SmartPtr"));
-   pTest->n = "222";
+   {
+      Ptest pTest (new test ("SmartPtr"));
+      pTest->n = "222";
+   }
+
    defineHndl (test);
+   defineHndl (test2);
    Htest hHandle;
    check (!hHandle.isDefined ());
 
    hHandle.define ();
    check (hHandle.isDefined ());
    hHandle->n = "Handle";
+   check (strlen (hHandle->n) == 6);
    check (hHandle.isDefined ());
    hHandle.undefine ();
    check (!hHandle.isDefined ());
@@ -66,6 +79,19 @@ int main (int argc, char* argv[]) {
    check (hHandle2.isDefined ());
    hHandle2 = hHandle;
    check (!hHandle2.isDefined ());
+
+   Htest2 derived;
+   derived.define ();
+   check (derived->len == 3);
+   derived->n = "1";
+   derived->len = strlen (derived->n);
+   check (derived->len == 1);
+   hHandle = Htest::cast (derived);
+   check (strlen (hHandle->n) == 1);
+   derived = Htest2::cast (hHandle);
+   check (derived.ptr () == hHandle.ptr ());
+   check (derived->len == 1);
+   check (Htest2::cast (hHandle)->len == 1);
 
    if (cErrors)
       std::cout << "Failures: " << cErrors << '\n';
