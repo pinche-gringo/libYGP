2212babac2849fa650f852b644c8d3517e5ef946
Markus Schwab <g17m0@lycos.com>
2006-04-23 02:59:45 +0000

    Re-written; does not derive from AttrVal anymore; Added castDynamic()

--- YGP/Handle.h
+++ YGP/Handle.h
@@ -1,7 +1,7 @@
 #ifndef HANDLE_H
 #define HANDLE_H
 
-// $Id: Handle.h,v 1.13 2005/01/10 02:16:31 markus Rel $
+// $Id: Handle.h,v 1.14 2006/04/23 02:59:45 markus Rel $
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -25,48 +25,6 @@
 
 namespace YGP {
 
-/**Smart-pointer-class with reference-counting; base-class for type-safe
-   handles (defined with a template at end of file)
-
-   The object is freed if there are no more references to it.
-
-   As every attribute, handles can be undefined (equivalent to NULL pointers).
-*/
-class IHandle : public AttributValue {
- public:
-   // Manager functions
-   /// Default constructor; creates an undefined handle
-   IHandle () : AttributValue () { }
-   /// Copy constructor; let's this point to the same object than other.
-   /// Increases the reference count of the object.
-   IHandle (const IHandle& other) : AttributValue (other) { }
-   virtual ~IHandle ();
-
-   IHandle& operator= (const IHandle& other);
-
-   virtual void undefine ();
-
-   /// Reads a value from the passed stream. Must not be called!
-   virtual void readFromStream (std::istream&) throw (std::invalid_argument) {
-      Check (0);
-   }
-
- protected:
-   // Interface to RefCount
-   virtual void create (void* pValue) = 0; ///< Method to create the object.
-   virtual void link () = 0;  ///< Method to link the handle to the object pointing to.
-   virtual void unlink () = 0;  ///< Method to unlink the handle from the object pointing to.
-   virtual void* getValue () const = 0;  ///< Method to access the object pointed to by the handle.
-   void          setValue (void* pValue);  ///< Method to set the object pointed to by the handle. This method cares about the reference count (and unlinks the old and links the new object). 
-
-   /// Method to set the object pointed to by the handle. This method does not
-   /// care about the reference count; use with care (or don't use at all!).
-   virtual void assignValue (void* pValue) = 0;
-
-   // Interface of AttributValue
-   virtual void define ();
-};
-
 
 template <class T> class Handle;
 
@@ -77,14 +35,12 @@ template <class T> class Handle;
    referenced object.
 */
 template <class T> class RefCount {
-   friend class Handle<T>;
-
  public:
    // Manager-functions
    /// Default constructor; creates a reference counting object pointing
    /// the passed object. The count is set to 1.
    RefCount (T* pData = NULL) : pValue (pData), count (1) { }
-   ~RefCount () { }                                             ///< Destructor
+   ~RefCount () { delete pValue;  }                             ///< Destructor
 
    // Connecting other references
    void link () { ++count; }                 ///< Increases the reference count
@@ -92,17 +48,20 @@ template <class T> class RefCount {
    /// is freed
    void unlink () { Check1 (count); if (!--count) delete this; }
 
+   T* getValue () const { return pValue; }
+
+
  private:
    // Prohibited manager functions:
    RefCount (const RefCount& other);
    RefCount& operator= (const RefCount& other);
 
-   SmartPtr<T> pValue;                       ///< Pointer to the handled object
+   T* pValue;                                ///< Pointer to the handled object
    unsigned int count;
 };
 
 
-/**Type-safe versions of IHandle (smart pointer with reference-counting)
+/**Type-safe versions of a smart pointer with reference-counting
 
    The object is freed if there are no more references to it.
 
@@ -115,45 +74,74 @@ template <class T> class RefCount {
    <tt>defineHndl(string)</tt> would create a <tt>typedef Handle<std::string>
    Hstring</tt>.
 
-   As every attribute, handles can be undefined (equivalent to NULL pointers).
+   Handles can be undefined (equivalent to NULL pointers).
 */
-template <class T> class Handle : public IHandle {
+template <class T> class Handle {
  public:
    /// Default constructor; creates an undefined handle of an object of type \c T
-   Handle () : IHandle (), pData (NULL) { }
+   Handle () : pData (NULL) { }
    /// Constructor which lets the handle point to the passed object. The
    /// handle is defined and the reference-count is set to 1.
-   Handle (T* pValue) : IHandle (), pData (new RefCount<T> (pValue)) { setDefined (); }
+   Handle (T* pValue) : pData (new RefCount<T> (pValue)) { }
    /// Copy constructor; let's this point to the same object than other.
    /// Increases the reference count of the object.
-   Handle (const Handle& other) : IHandle (other), pData (other.pData) {
-      if (isDefined ()) link ();
-      Check2 (isDefined () ? pData != NULL : pData == NULL); }
-   ~Handle () { if (isDefined ()) unlink (); }  ///< Destructor; unlinks object
+   Handle (const Handle<T>& other) : pData (other.pData) {
+      if (pData) pData->link (); }
+   /// The templated contstructor allows copy construction from any object
+   /// that's castable.  Thus, it does downcasts.
+   template <class T_CastFrom>
+   Handle (const Handle<T_CastFrom>& src) : pData (src.pData) {
+      if (pData) pData->link (); }
+   ~Handle () { if (pData) pData->unlink (); }  ///< Destructor; unlinks object
+
 
    /// Assignment operator; both handles points to the same object and the
    /// reference-counter is increased. The count of the object referenced
    /// before is decreased.
    Handle& operator= (const Handle& other) {
-      return (Handle&)IHandle::operator= (other); }
+      if (pData != other.pData) {
+	 if (pData) pData->unlink ();
+	 pData = other.pData;
+	 if (pData) pData->link (); }
+      return *this;
+   }
+   /// The templated assignment operator allows copy construction from any
+   /// object that's castable.  Thus, it does downcasts.
+   template <class T_CastFrom>
+   Handle& operator= (const Handle<T_CastFrom> other) {
+      if (pData != other.pData) {
+	 if (pData) pData->unlink ();
+	 pData = other.pData;
+	 if (pData) pData->link (); }
+   }
 
    /// Defines the handle and creates a new object pointed to (with
    /// reference-count 1). If an already defined handle is defined again, its
    /// old object is released (and freed if the reference-count becomes 0) and
    /// a new object is assigned.
-   virtual void define () { IHandle::setValue (new T); }
+   void define () {
+      if (pData) pData->unlink ();
+      pData = new RefCount<T> (new T); }
+   bool isDefined () const { return pData; }  ///< Checks if the object is defined
+   /// Undefines the object
+   void undefine () {
+      if (pData) {
+	 pData->unlink ();
+	 pData = NULL;
+      }
+   }
 
    /// \name Accessing the object
    //@{
    /// Casting to a <tt>T*</tt>
    /// \returns T*: Pointer to the object
-   T* ptr        () const { return pData ? (T*)pData->pValue : NULL; }
-   T* operator-> () const { return pData->pValue; }  ///< Dereferencing the pointer
-   /// T& operator*  () const { return *pData->pValue; } ///< Dereferencing the pointer
-   operator T*   () const { return ptr (); }       ///< Casting to a <tt>T*</tt>
+   T* ptr        () const { return pData ? pData->getValue () : NULL; }
+   T* operator-> () const { return pData->getValue (); }    ///< Dereferencing
+   operator T*   () const { return ptr (); }      ///< Casting to a <tt>T*</tt>
+   RefCount<T>* getData () const { return pData; }  ///< Return RefCount-object
    //@}
 
-   /// Cast to derived class.
+   /// (Static) Cast to derived class.
    ///
    /// The Handle can't be cast with the usual notation so instead you can use
    /// @code
@@ -162,14 +150,33 @@ template <class T> class Handle : public IHandle {
    ///
    template <class T_CastFrom>
    static Handle<T> cast (const Handle<T_CastFrom>& src) {
-      Handle<T> h;
-      if (src.isDefined ()) {
-	 h.setDefined ();
-	 Check3 (src.pData);
-	 h.pData = (RefCount<T>*)(src.pData);
-	 h.pData->link ();
+      Handle<T> res;
+      T* ptr (static_cast<T*> (src.ptr ()));
+      if (ptr) {
+	 res.pData = (RefCount<T>*)(void*)src.getData ();
+	 if (res.pData)
+	    res.pData->link ();
       }
-      return h;
+      return res;
+   }
+
+   /// Dynamic cast to derived class.
+   ///
+   /// The Handle can't be cast with the usual notation so instead you can use
+   /// @code
+   ///   ptr_derived = Handle<Derived>::castDynamic (ptr_base);
+   /// @endcode
+   ///
+   template <class T_CastFrom>
+   static Handle<T> castDynamic (const Handle<T_CastFrom>& src) {
+      Handle<T> res;
+      T* ptr (dynamic_cast<T*> (src.ptr ()));
+      if (ptr) {
+	 res.pData = (RefCount<T>*)(void*)(src.getData ());
+	 if (res.pData)
+	    res.pData->link ();
+      }
+      return res;
    }
 
    /// \name Comparison
@@ -178,26 +185,17 @@ template <class T> class Handle : public IHandle {
    /// \param other: Handle to compare with
    /// \returns bool: True if objects are equal
    bool operator== (const Handle& other) const {
-      return pData->pValue == other.pData->pValue; }
+      return ptr () == other.ptr (); }
    /// Comparison-operator; Checks if two handles are equal
    /// \param other: Handle to compare with
    /// \returns bool: True if objects are equal
    bool operator!= (const Handle& other) const {
-      return pData->pValue != other.pData->pValue; }
+      return pData->getValue () != other.pData->getValue (); }
+
    //@}
 
- protected:
-   /// \name Interface to RefCount
-   //@{
-   virtual void create (void* pValue) { pData = new RefCount<T> ((T*)pValue); }
-   virtual void link () { Check2 (pData); pData->link (); }
-   virtual void unlink () { Check2 (pData); pData->unlink (); }
-   virtual void* getValue () const { return pData; }
-   virtual void assignValue (void* pValue) { pData = (RefCount<T>*)pValue; }
-   //@}
-
- public:
-   RefCount<T>* pData; ///< Don't access! Should be private; but fails to compile
+ private:
+   RefCount<T>* pData;               ///< Pointer to (reference-counted) object
 };
 
 #define defineHndl(class)         typedef YGP::Handle<class> H##class
