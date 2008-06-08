3d13cd1d749e0d1c42e51b2917f72a56060598dd
Markus Schwab <g17m0@lycos.com>
2008-06-08 12:32:07 +0000

    Put under GPL-3

--- CGP/CIDirSrch.h
+++ CGP/CIDirSrch.h
@@ -1,21 +1,22 @@
 #ifndef CGP_CIDIRCOMP_H
 #define CGP_CIDIRCOMP_H
 
-//$Id: CIDirSrch.h,v 1.9 2008/05/18 13:21:27 markus Rel $
+//$Id: CIDirSrch.h,v 1.10 2008/06/08 12:32:07 markus Rel $
 
-// This program is free software; you can redistribute it and/or modify
+// This file is part of libYGP.
+//
+// libYGP is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
+//
+// libYGP is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <CORBA.h>
 
--- CGP/Version.cpp
+++ CGP/Version.cpp
@@ -1,28 +1,29 @@
-//$Id: Version.cpp,v 1.3 2004/01/15 06:26:40 markus Rel $
+//$Id: Version.cpp,v 1.4 2008/06/08 12:32:07 markus Rel $
 
 //PROJECT     : General/CORBA
 //SUBSYSTEM   : CDirSrch
 //REFERENCES  :
 //TODO        :
 //BUGS        :
-//REVISION    : $Revision: 1.3 $
+//REVISION    : $Revision: 1.4 $
 //AUTHOR      : Markus Schwab
 //CREATED     : 19.1.2001
-//COPYRIGHT   : Copyright (C) 2001 - 2004
+//COPYRIGHT   : Copyright (C) 2001 - 2004, 2008
 
-// This program is free software; you can redistribute it and/or modify
+// This file is part of libYGP.
+//
+// libYGP is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
+//
+// libYGP is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <ygp-cfg.h>
 
