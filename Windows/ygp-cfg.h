c3b0699ad38d470e0addb83679c4d5bf372d2067
Markus Schwab <g17m0@lycos.com>
2008-06-08 12:42:40 +0000

    Put under GPL version 3

--- Windows/ygp-cfg.h
+++ Windows/ygp-cfg.h
@@ -1,21 +1,22 @@
 #ifndef YGP_CFG_H
 #define YGP_CFG_H
 
-//$Id: ygp-cfg.h,v 1.6 2003/12/10 00:20:35 markus Rel $
+//$Id: ygp-cfg.h,v 1.7 2008/06/08 12:45:13 markus Rel $
 
-// This program is free software; you can redistribute it and/or modify
+// This file is part of libYGP.
+//
+// libYGP is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
+//
+// libYGP is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
 // Detect common platforms
 #define WINDOWS 0
