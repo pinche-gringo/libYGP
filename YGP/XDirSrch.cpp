92647d4d42d19ee90653ed7413d00bdd71b5edce
Markus Schwab <g17m0@users.sourceforge.net>
2009-11-08 19:35:38 +0100

    YGP/XDirSrch.cpp: Using boost::tokenizer instead of YGP::Tokenize

--- YGP/XDirSrch.cpp
+++ YGP/XDirSrch.cpp
@@ -8,7 +8,7 @@
 //REVISION    : $Revision: 1.14 $
 //AUTHOR      : Markus Schwab
 //CREATED     : 17.10.2002
-//COPYRIGHT   : Copyright (C) 2002 - 2004, 2007, 2008
+//COPYRIGHT   : Copyright (C) 2002 - 2004, 2007 - 2009
 
 // This file is part of libYGP.
 //
@@ -28,13 +28,19 @@
 
 #include <string>
 
+#include <boost/tokenizer.hpp>
+
+#include "YGP/Path.h"
 #include "YGP/Check.h"
 #include "YGP/Trace.h"
 #include "YGP/FileRExp.h"
-#include "YGP/PathSrch.h"
 
 
 namespace YGP {
+
+
+typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+
 
 //-----------------------------------------------------------------------------
 /// Checks the validity of the filename
@@ -51,21 +57,18 @@ bool _XDSfileIsValid (const std::string& files, const char* pFile) {
    if (files.empty ())
       return true;
 
-   PathSearch  list (files);
-   std::string node;
 
    FileRegularExpr regexp (NULL);
    bool include (false);
 
    // Test every file in list
-   while ((node = list.getNextNode ()).size ()) {
-      TRACE8 ("_XDSfileIsValid (const string&, const char*) const - Node: " << node);
-      Check3 ((node[0] == 'i') || (node[0] == 'x'));
+   tokenizer list (files, boost::char_separator<char> (YGP::Path::SEPARATOR_STR));
+   for (tokenizer::iterator i (list.begin ()); i != list.end (); ++i) {
+      TRACE8 ("_XDSfileIsValid (const string&, const char*) const - Node: " << *i);
+      Check3 ((i->at (0) == 'i') || (i->at (0) == 'x'));
+      include = (i->at (0) == 'i');
 
-      include = node[0] == 'i';
-      node.replace (0, 1, 0, '\0');
-
-      regexp = node.c_str ();
+      regexp = i->substr (1).c_str ();
       if (regexp.matches (pFile))                      // Test if file matches
           return include;
    } // end-while
@@ -74,8 +77,8 @@ bool _XDSfileIsValid (const std::string& files, const char* pFile) {
 }
 
 //-----------------------------------------------------------------------------
-/// Appends a (list of) node to a list. Every subnode in the node is prefixed
-/// with the prefix-character
+/// Prefixes a list with another list of nodes. Every subnode of the
+/// node is prefixed with the specified prefix-character
 /// \param list List to change
 /// \param prefix Leading character
 /// \param node Node to check
@@ -86,11 +89,11 @@ void _XDSaddNode (std::string& list, char prefix, const std::string& node) {
            << node);
    Check1 ((prefix == 'i') || (prefix == 'x'));
 
-   PathSearch  l (node);
    std::string temp;
 
-   while ((temp = l.getNextNode ()).size ())
-      list = prefix + temp + std::string (1, PathSearch::PATHSEPARATOR) + list;
+   tokenizer val (node, boost::char_separator<char> (YGP::Path::SEPARATOR_STR));
+   for (tokenizer::iterator i (val.begin ()); i != val.end (); ++i)
+      list = prefix + (*i) + std::string (1, Path::SEPARATOR) + list;
 }
 
 }
