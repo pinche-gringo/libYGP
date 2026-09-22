f8a114536ca807ddd3da71678db68ca0dbf82156
Markus Schwab <markus.schwab.32@gmail.com>
2026-09-22 18:06:22 +0200

    YGP/Thread: Re-formatted

--- YGP/Thread.cpp
+++ YGP/Thread.cpp
@@ -1,14 +1,11 @@
-//$Id: Thread.cpp,v 1.27 2008/06/10 21:47:20 markus Rel $
-
-//PROJECT     : libYGP
-//SUBSYSTEM   : Thread
-//REFERENCES  :
-//TODO        :
-//BUGS        :
-//REVISION    : $Revision: 1.27 $
-//AUTHOR      : Markus Schwab
-//CREATED     : 28.4.2002
-//COPYRIGHT   : Copyright (C) 2002 - 2004, 2006, 2008
+// PROJECT     : libYGP
+// SUBSYSTEM   : Thread
+// REFERENCES  :
+// TODO        :
+// BUGS        :
+// AUTHOR      : Markus Schwab
+// CREATED     : 28.4.2002
+// COPYRIGHT   : Copyright (C) 2002 - 2004, 2006, 2008, 2026
 
 // This file is part of libYGP.
 //
@@ -25,11 +22,9 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #ifdef _MSC_VER
-#pragma warning(disable:4786) // disable warning about truncating debug info
+#    pragma warning(disable : 4786) // disable warning about truncating debug info
 #endif
-
 
 #include "YGP/Internal.h"
 
@@ -45,42 +40,44 @@
 #include "YGP/Thread.h"
 
 #if HAVE_LIBPTHREAD
-#   include <unistd.h>
-#   include <sys/wait.h>
+#    include <sys/wait.h>
+#    include <unistd.h>
 #else
-#  ifdef HAVE_BEGINTHREAD
-#     include <process.h>
-#     include <map>
-#     include "YGP/Mutex.h"
+#    ifdef HAVE_BEGINTHREAD
+#        include "YGP/Mutex.h"
+#        include <map>
+#        include <process.h>
 
-      // Map of wait-mutexes (1 per thread
-      static std::map<unsigned long, YGP::Mutex> mutexes;
-      static std::map<unsigned long, void*> rcs;
-#  endif
-#  include <cstdio>
+// Map of wait-mutexes (1 per thread
+static std::map<unsigned long, YGP::Mutex> mutexes;
+static std::map<unsigned long, void*> rcs;
+#    endif
+#    include <cstdio>
 #endif
-
 
 namespace YGP {
 
 //-----------------------------------------------------------------------------
 /// Defaultconstructor; create the object but no actual thread
 //-----------------------------------------------------------------------------
-Thread::Thread ()
-   : pArgs_ (NULL)
+Thread::Thread()
+    : pArgs_(NULL)
 #ifdef HAVE_LIBPTHREAD
-   , id (0)
+      ,
+      id(0)
 #else
-   , canceled (false)
-#  ifdef HAVE_BEGINTHREAD
-   , callback (NULL)
-   , id (0)
-#  else
-   , id (0)
-#  endif
+      ,
+      canceled(false)
+#    ifdef HAVE_BEGINTHREAD
+      ,
+      callback(NULL), id(0)
+#    else
+      ,
+      id(0)
+#    endif
 #endif
- {
-   TRACE3 ("Thread::Thread ()");
+{
+    TRACE3("Thread::Thread()");
 }
 
 //-----------------------------------------------------------------------------
@@ -89,25 +86,23 @@ Thread::Thread ()
 /// \param pArgs Pointer to argument(s)
 /// \throw YGP::ExecError describing the error
 //-----------------------------------------------------------------------------
-Thread::Thread (THREAD_FUNCTION fnc, void* pArgs)
-   : pArgs_ (pArgs)
+Thread::Thread(THREAD_FUNCTION fnc, void* pArgs)
+    : pArgs_(pArgs)
 #ifdef HAVE_LIBPTHREAD
-   , id ()
+      ,
+      id()
 #endif
 {
-   TRACE3 ("Thread::Thread (THREAD_FUNCTION, void*)");
-   init (fnc, pArgs);
+    TRACE3("Thread::Thread(THREAD_FUNCTION, void*)");
+    init(fnc, pArgs);
 
-   TRACE9 ("Thread::Thread (THREAD_FUNCTION, void*) -id = " << (int)id);
+    TRACE9("Thread::Thread(THREAD_FUNCTION, void*) -id = " << (int)id);
 }
 
 //-----------------------------------------------------------------------------
 /// Destructor
 //-----------------------------------------------------------------------------
-Thread::~Thread () {
-   TRACE3 ("Thread::~Thread ()");
-}
-
+Thread::~Thread() { TRACE3("Thread::~Thread()"); }
 
 //-----------------------------------------------------------------------------
 /// Creates the actual thread from the passed function
@@ -115,49 +110,49 @@ Thread::~Thread () {
 /// \param pArgs Pointer to parameters
 /// \throw YGP::ExecError describing the error
 //-----------------------------------------------------------------------------
-void Thread::init (THREAD_FUNCTION fnc, void* pArgs) {
+void Thread::init(THREAD_FUNCTION fnc, void* pArgs) {
 #ifdef HAVE_LIBPTHREAD
-   if (pthread_create (&id, NULL, fnc, pArgs) != 0) {
-#elif  defined (HAVE_BEGINTHREAD)
-   callback = fnc;
-   mutexes[id = _beginthread (threadFunction, 0, this)].lock ();
-   if (id == -1U) {
-      mutexes[-1U].unlock ();
+    if (pthread_create(&id, NULL, fnc, pArgs) != 0) {
+#elif defined(HAVE_BEGINTHREAD)
+    callback = fnc;
+    mutexes[id = _beginthread(threadFunction, 0, this)].lock();
+    if (id == -1U) {
+        mutexes[-1U].unlock();
 #endif
 
-#if defined (HAVE_LIBPTHREAD) || defined (HAVE_BEGINTHREAD)
-      std::string err (_("Can't create thread!\nReason: %1"));
-      err.replace (err.find ("%1"), 2, strerror (errno));
-      throw YGP::ExecError (err);
-   }
+#if defined(HAVE_LIBPTHREAD) || defined(HAVE_BEGINTHREAD)
+        std::string err(_("Can't create thread!\nReason: %1"));
+        err.replace(err.find("%1"), 2, strerror(errno));
+        throw YGP::ExecError(err);
+    }
 #else
-   canceled = false;
-   id = fork ();
-   switch (id) {
-   case 0:                                  // Child-part: Call passed function
-      ret (fnc (this));
+        canceled = false;
+        id = fork();
+        switch (id) {
+        case 0: // Child-part: Call passed function
+            ret(fnc(this));
 
-   case -1: {                        // Error creating process: Throw exception
-      std::string err (_("Can't create background-process!\nReason: %1"));
-      err.replace (err.find ("%1"), 2, strerror (errno));
-      throw YGP::ExecError (err);
-      }
-   } // end-switch
+        case -1: { // Error creating process: Throw exception
+            std::string err(_("Can't create background-process!\nReason: %1"));
+            err.replace(err.find("%1"), 2, strerror(errno));
+            throw YGP::ExecError(err);
+        }
+        } // end-switch
 #endif
-   TRACE9 ("Thread::init (THREAD_FUNCTION) -id = " << (int)id);
+    TRACE9("Thread::init(THREAD_FUNCTION) -id = " << (int)id);
 }
 
 //-----------------------------------------------------------------------------
 /// Sets the return value for the thread
 /// \param rc Returncode
 //-----------------------------------------------------------------------------
-void Thread::ret (void* rc) const {
+void Thread::ret(void* rc) const {
 #ifdef HAVE_LIBPTHREAD
-   pthread_exit (rc);
+    pthread_exit(rc);
 #elif defined HAVE_BEGINTHREAD
-   rcs[id] = rc;
+        rcs[id] = rc;
 #else
-   _exit (rc ? *static_cast<int*> (rc) : -1);
+    _exit(rc ? *static_cast<int*>(rc) : -1);
 #endif
 }
 
@@ -167,11 +162,11 @@ void Thread::ret (void* rc) const {
 ///     thread; in such a case the thread must check itself, if it should be
 ///     canceled.
 //-----------------------------------------------------------------------------
-void Thread::cancel () {
+void Thread::cancel() {
 #ifdef HAVE_LIBPTHREAD
-   pthread_cancel (id);
+    pthread_cancel(id);
 #else
-   canceled = true;
+        canceled = true;
 #endif
 }
 
@@ -181,9 +176,9 @@ void Thread::cancel () {
 ///     thread; in such a case the thread must check itself, if it should be
 ///     canceled.
 //-----------------------------------------------------------------------------
-void Thread::allowCancelation (bool allow) {
+void Thread::allowCancelation(bool allow) {
 #ifdef HAVE_LIBPTHREAD
-   pthread_setcancelstate (allow ? PTHREAD_CANCEL_ENABLE : PTHREAD_CANCEL_DISABLE, NULL);
+    pthread_setcancelstate(allow ? PTHREAD_CANCEL_ENABLE : PTHREAD_CANCEL_DISABLE, NULL);
 #endif
 }
 
@@ -192,12 +187,12 @@ void Thread::allowCancelation (bool allow) {
 /// \param id Thread to wait for
 /// \returns void* The returncode of the thread
 //-----------------------------------------------------------------------------
-void* Thread::waitForThread (const Thread& id) {
+void* Thread::waitForThread(const Thread& id) {
 #ifndef HAVE_BEGINTHREAD
-    return waitForThread ((unsigned long)id.id);
+    return waitForThread((unsigned long)id.id);
 #else
-   waitForThread (id.id);
-   return rcs[id.id];
+        waitForThread(id.id);
+        return rcs[id.id];
 #endif
 }
 
@@ -206,35 +201,35 @@ void* Thread::waitForThread (const Thread& id) {
 /// \param id Thread to wait for
 /// \returns void* The returncode of the thread
 //-----------------------------------------------------------------------------
-void* Thread::waitForThread (unsigned long id) {
-   TRACE3 ("Thread::waitForThread (unsigned long) - " << id);
+void* Thread::waitForThread(unsigned long id) {
+    TRACE3("Thread::waitForThread(unsigned long) - " << id);
 
 #ifdef HAVE_LIBPTHREAD
-   void* rc (NULL);
-   pthread_join ((pthread_t)id, &rc);
-   return rc;
+    void* rc(NULL);
+    pthread_join((pthread_t)id, &rc);
+    return rc;
 #elif defined HAVE_BEGINTHREAD
-   mutexes[id].lock ();
-   mutexes[id].unlock ();
-   return rcs[id];
+        mutexes[id].lock();
+        mutexes[id].unlock();
+        return rcs[id];
 #else
-   int rc (0);
-   waitpid (id, &rc, 0);
-   return (void*)rc;
+    int rc(0);
+    waitpid(id, &rc, 0);
+    return (void*)rc;
 #endif
 }
 
 //-----------------------------------------------------------------------------
 /// Checks if the thread should be canceled and does so, if yes
 //-----------------------------------------------------------------------------
-void Thread::isToCancel () const {
+void Thread::isToCancel() const {
 #ifdef HAVE_LIBPTHREAD
-   pthread_testcancel ();
+    pthread_testcancel();
 #elif defined HAVE_BEGINTHREAD
-   ExitThread (-1U);
+        ExitThread(-1U);
 #else
-   if (canceled)
-      _exit (-1);
+    if (canceled)
+        _exit(-1);
 #endif
 }
 
@@ -244,12 +239,12 @@ void Thread::isToCancel () const {
 /// for a thread-function and can't (of course) not cast it
 /// \param params Pointer to thread
 //-----------------------------------------------------------------------------
-void Thread::threadFunction (void* params) {
-   Thread* pThread = reinterpret_cast<Thread*> (params);
-   Check1 (pThread);
-   rcs[pThread->id] = pThread->callback (pThread);
-   mutexes[pThread->id].unlock ();
+void Thread::threadFunction(void* params) {
+    Thread* pThread = reinterpret_cast<Thread*>(params);
+    Check1(pThread);
+    rcs[pThread->id] = pThread->callback(pThread);
+    mutexes[pThread->id].unlock();
 }
 #endif
 
-}
+} // namespace YGP
--- YGP/Thread.h
+++ YGP/Thread.h
@@ -1,8 +1,6 @@
 #ifndef YGP_THREAD_H
 #define YGP_THREAD_H
 
-//$Id: Thread.h,v 1.24 2008/05/18 13:21:27 markus Rel $
-
 // This file is part of libYGP.
 //
 // libYGP is free software: you can redistribute it and/or modify
@@ -18,30 +16,28 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include <ygp-cfg.h>
 
 #ifdef HAVE_LIBPTHREAD
-#  include <pthread.h>
+#    include <pthread.h>
 #else
-#  if !defined (HAVE_BEGINTHREAD) && defined (HAVE__BEGINTHREAD)
-#     define HAVE_BEGINTHREAD
-#     define beginthread    _beginthread
-#     define endthread      _endthread
-#  endif
+#    if !defined(HAVE_BEGINTHREAD) && defined(HAVE__BEGINTHREAD)
+#        define HAVE_BEGINTHREAD
+#        define beginthread _beginthread
+#        define endthread _endthread
+#    endif
 
-#  if HAVE_WINDOWS_H
-#     define WIN32_LEAN_AND_MEAN
-#     include <windows.h>
-      typedef void* (WINTHREAD_FUNCTION) (void*);
-#  elif !defined (HAVE_BEGINTHREAD)
-#      include <unistd.h>
-#      include <sys/types.h>
-#   endif
+#    if HAVE_WINDOWS_H
+#        define WIN32_LEAN_AND_MEAN
+#        include <windows.h>
+typedef void*(WINTHREAD_FUNCTION)(void*);
+#    elif !defined(HAVE_BEGINTHREAD)
+#        include <sys/types.h>
+#        include <unistd.h>
+#    endif
 #endif
 
 #include <YGP/Exception.h>
-
 
 namespace YGP {
 
@@ -53,81 +49,81 @@ namespace YGP {
    controlling the thread.
 */
 class Thread {
- public:
-   /// Declaration of prototype of callback.
-   typedef void* (*THREAD_FUNCTION) (void*);
+  public:
+    /// Declaration of prototype of callback.
+    typedef void* (*THREAD_FUNCTION)(void*);
 
-   virtual ~Thread ();
+    virtual ~Thread();
 
-   /// Creates a new thread; the argument is passed directly to the thread function
-   /// \param fnc Thread function to execute
-   /// \param pArgs Argument to the thread
-   static Thread* create (THREAD_FUNCTION fnc, void* pArgs) {
-      return new Thread (fnc, pArgs); }
-   /// Creates a new thread; a pointer to the thread ID is passed to the thread function
-   /// \param fnc Thread function to execute
-   /// \param pArgs Argument to the thread
-   static Thread* create2 (THREAD_FUNCTION fnc, void* pArgs) {
-      Thread* t = new Thread;
-      t->pArgs_ = pArgs;
-      t->init (fnc, t);
-      return t; }
+    /// Creates a new thread; the argument is passed directly to the thread function
+    /// \param fnc Thread function to execute
+    /// \param pArgs Argument to the thread
+    static Thread* create(THREAD_FUNCTION fnc, void* pArgs) { return new Thread(fnc, pArgs); }
+    /// Creates a new thread; a pointer to the thread ID is passed to the thread function
+    /// \param fnc Thread function to execute
+    /// \param pArgs Argument to the thread
+    static Thread* create2(THREAD_FUNCTION fnc, void* pArgs) {
+        Thread* t = new Thread;
+        t->pArgs_ = pArgs;
+        t->init(fnc, t);
+        return t;
+    }
 
-   /// Returns the argument passed to the thread
-   void* getArgs () const { return pArgs_; }
+    /// Returns the argument passed to the thread
+    void* getArgs() const { return pArgs_; }
 
-   /// \name Termination of the thread
-   //@{
-   void allowCancelation (bool allow = true);
-   void cancel ();
-   static void* waitForThread (const Thread& id);
-   static void* waitForThread (unsigned long threadID);
-   void isToCancel () const;
-   //@}
+    /// \name Termination of the thread
+    //@{
+    void allowCancelation(bool allow = true);
+    void cancel();
+    static void* waitForThread(const Thread& id);
+    static void* waitForThread(unsigned long threadID);
+    void isToCancel() const;
+    //@}
 
-   /// Get the ID of the thread
-   unsigned long getID () const { return (unsigned long)id; }
-   /// Get the ID of the currently running thread
-   static unsigned long currentID () {
+    /// Get the ID of the thread
+    unsigned long getID() const { return (unsigned long)id; }
+    /// Get the ID of the currently running thread
+    static unsigned long currentID() {
 #ifdef HAVE_LIBPTHREAD
-      return (unsigned long)pthread_self ();
+        return (unsigned long)pthread_self();
 #elif defined HAVE_BEGINTHREAD
-#  ifdef HAVE_WINDOWS_H
-      return (unsigned long)GetCurrentThreadId ();
-#  else
-#     warning Method Thread::currentID () does not work!
-      return (unsigned long)-1;
-#  endif
+#    ifdef HAVE_WINDOWS_H
+        return (unsigned long)GetCurrentThreadId();
+#    else
+#        warning Method Thread::currentID () does not work!
+        return (unsigned long)-1;
+#    endif
 #else
-      return (unsigned long)getpid ();
+        return (unsigned long)getpid();
 #endif
-   }
+    }
 
- protected:
-   Thread ();
-   Thread (THREAD_FUNCTION fnc, void* pArgs);
+  protected:
+    Thread();
+    Thread(THREAD_FUNCTION fnc, void* pArgs);
 
-   void ret (void* rc) const;
-   void init (THREAD_FUNCTION fnc, void* pArgs);
+    void ret(void* rc) const;
+    void init(THREAD_FUNCTION fnc, void* pArgs);
 
-   void* pArgs_;             ///< Pointer to (array of) arguments to the thread
+    void* pArgs_; ///< Pointer to (array of) arguments to the thread
 
- private:
-   Thread (const Thread&);
-   Thread& operator= (const Thread&);
+  private:
+    Thread(const Thread&);
+    Thread& operator=(const Thread&);
 
 #ifdef HAVE_LIBPTHREAD
-   pthread_t id;
+    pthread_t id;
 #else
-   bool  canceled;
+    bool canceled;
 
-#  ifdef HAVE_BEGINTHREAD
-   THREAD_FUNCTION callback;
-   static void threadFunction (void* thread);
-   unsigned long id;
-#  else
-   pid_t id;
-#  endif
+#    ifdef HAVE_BEGINTHREAD
+    THREAD_FUNCTION callback;
+    static void threadFunction(void* thread);
+    unsigned long id;
+#    else
+    pid_t id;
+#    endif
 #endif
 };
 
@@ -139,56 +135,54 @@ class Thread {
    controlling the thread.
 */
 template <class T> class OThread : public Thread {
- public:
-   /// Declaration of type of callback
-   typedef void* (T::*THREAD_OBJMEMBER) (void*);
+  public:
+    /// Declaration of type of callback
+    typedef void* (T::*THREAD_OBJMEMBER)(void*);
 
-   /// Destructor
-   ~OThread () { }
+    /// Destructor
+    ~OThread() {}
 
-   /// Creates a new thread; the argument is passed directly to the thread function
-   /// \param obj Object having a member to execute in a thread
-   /// \param fnc Member to execute as thread
-   /// \param pArgs Argument to the thread
-   static OThread<T>* create (T* obj, THREAD_OBJMEMBER fnc, void* pArgs) {
-      return new OThread<T> (obj, fnc, pArgs); }
-   /// Creates a new thread; a pointer to the thread is passed to the thread function
-   /// \param obj Object having a member to execute in a thread
-   /// \param fnc Member to execute as thread
-   /// \param pArgs Argument to the thread
-   static OThread<T>* create2 (T* obj, THREAD_OBJMEMBER fnc, void* pArgs) {
-      return new OThread<T> (obj, fnc, pArgs, true); }
+    /// Creates a new thread; the argument is passed directly to the thread function
+    /// \param obj Object having a member to execute in a thread
+    /// \param fnc Member to execute as thread
+    /// \param pArgs Argument to the thread
+    static OThread<T>* create(T* obj, THREAD_OBJMEMBER fnc, void* pArgs) { return new OThread<T>(obj, fnc, pArgs); }
+    /// Creates a new thread; a pointer to the thread is passed to the thread function
+    /// \param obj Object having a member to execute in a thread
+    /// \param fnc Member to execute as thread
+    /// \param pArgs Argument to the thread
+    static OThread<T>* create2(T* obj, THREAD_OBJMEMBER fnc, void* pArgs) { return new OThread<T>(obj, fnc, pArgs, true); }
 
+  protected:
+    /// Constructor
+    /// \param obj Object having a member to execute in a thread
+    /// \param fnc Member to execute as thread
+    /// \param pArgs Argument to the thread
+    /// \param threadAsArg Flag, if the thread expects its argument directly
+    OThread(T* obj, THREAD_OBJMEMBER fnc, void* pArgs, bool threadAsArg = false)
+        : Thread(), indirect(threadAsArg), object(obj), callback(fnc) {
+        // Don't create Thread directly with data, because the thread might start
+        // without object and callback being initialized!!
+        pArgs_ = pArgs;
+        init(&proxy, this);
+    }
 
- protected:
-   /// Constructor
-   /// \param obj Object having a member to execute in a thread
-   /// \param fnc Member to execute as thread
-   /// \param pArgs Argument to the thread
-   /// \param threadAsArg Flag, if the thread expects its argument directly
-   OThread (T* obj, THREAD_OBJMEMBER fnc, void* pArgs, bool threadAsArg = false)
-      : Thread (), indirect (threadAsArg), object (obj), callback (fnc) {
-      // Don't create Thread directly with data, because the thread might start
-      // without object and callback being initialized!!
-      pArgs_ = pArgs;
-      init (&proxy, this); }
+  private:
+    OThread(const OThread&);
+    OThread& operator=(const OThread&);
 
- private:
-   OThread (const OThread&);
-   OThread& operator= (const OThread&);
+    static void* proxy(void* pArgs) {
+        OThread<T>* thread = static_cast<OThread<T>*>(pArgs);
+        void* rc = ((thread->object)->*(thread->callback))(thread->indirect ? thread : thread->getArgs());
+        delete thread;
+        return rc;
+    }
 
-   static void* proxy (void* pArgs) {
-      OThread<T>* thread = static_cast <OThread<T>*> (pArgs);
-      void* rc = ((thread->object)->*(thread->callback))
-         (thread->indirect ? thread : thread->getArgs ());
-      delete thread;
-      return rc; }
-
-   bool indirect;
-   T* object;
-   THREAD_OBJMEMBER callback;
+    bool indirect;
+    T* object;
+    THREAD_OBJMEMBER callback;
 };
 
-}
+} // namespace YGP
 
 #endif
