182e53222c45bf3fb64b3f7f05eafb1271d77750
Markus Schwab <markus.schwab.32@gmail.com>
2026-09-22 18:06:44 +0200

    YGP/Thread: Re-formmatted

--- YGP/Trace.h
+++ YGP/Trace.h
@@ -1,8 +1,6 @@
 #ifndef YGP_TRACE_H
 #define YGP_TRACE_H
 
-//$Id: Trace.h,v 1.14 2008/05/18 13:21:27 markus Rel $
-
 // This file is part of libYGP.
 //
 // libYGP is free software: you can redistribute it and/or modify
@@ -18,7 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 // Semantic of the traces:
 // There are 3 different types of traces: Only text, with a leading
 // time (including milliseconds) or with a leading timestamp (date and
@@ -31,112 +28,110 @@
 // TRACE is defined, when TRACELEVEL* is defined;
 // TRACE0 - TRACE9 only if TRACELVEL* is set to a value corresponding to TRACEx
 
-
 #ifdef TRACELEVEL_TS
-#  undef TRACELEVEL
-#  define TRACELEVEL TRACELEVEL_TS
+#    undef TRACELEVEL
+#    define TRACELEVEL TRACELEVEL_TS
 
-#  include <sys/time.h>
-#  include <ctime>
-#  include <iomanip>
-#  include <iostream>
+#    include <ctime>
+#    include <iomanip>
+#    include <iostream>
+#    include <sys/time.h>
 
-static inline void printTime () {
-   struct timeval actTime;
-   gettimeofday (&actTime, NULL);
-   static char time[24];
-   strftime (time, sizeof (time), "%x %X.", localtime (&actTime.tv_sec));
-   std::cout << time << std::setfill ('0') << std::setw (6) << actTime.tv_usec << ": ";
+static inline void printTime() {
+    struct timeval actTime;
+    gettimeofday(&actTime, NULL);
+    static char time[24];
+    strftime(time, sizeof(time), "%x %X.", localtime(&actTime.tv_sec));
+    std::cout << time << std::setfill('0') << std::setw(6) << actTime.tv_usec << ": ";
 }
 
-#elif defined (TRACELEVEL_TIME)
-#  undef TRACELEVEL
-#  define TRACELEVEL TRACELEVEL_TIME
+#elif defined(TRACELEVEL_TIME)
+#    undef TRACELEVEL
+#    define TRACELEVEL TRACELEVEL_TIME
 
-#  include <sys/time.h>
-#  include <ctime>
-#  include <iostream>
-#  include <iomanip>
+#    include <ctime>
+#    include <iomanip>
+#    include <iostream>
+#    include <sys/time.h>
 
-static inline void printTime () {
-   struct timeval actTime;
-   gettimeofday (&actTime, NULL);
-   static char time[12];
-   strftime (time, sizeof (time), "%X.", localtime (&actTime.tv_sec));
-   std::cout << time << std::setfill ('0') << std::setw (6) << actTime.tv_usec << ": ";
+static inline void printTime() {
+    struct timeval actTime;
+    gettimeofday(&actTime, NULL);
+    static char time[12];
+    strftime(time, sizeof(time), "%X.", localtime(&actTime.tv_sec));
+    std::cout << time << std::setfill('0') << std::setw(6) << actTime.tv_usec << ": ";
 }
 
 #else
-static inline void printTime () {
-}
+static inline void printTime() {}
 #endif
 
 #ifdef TRACELEVEL
-#include <iomanip>
-#include <iostream>
+#    include <iomanip>
+#    include <iostream>
 
 // Flush every trace imediately
-#define TRACE(x)            printTime (), std::cout << x << std::endl;
+#    define TRACE(x) printTime(), std::cout << x << std::endl;
 #endif
 
-#if defined (TRACELEVEL) && (TRACELEVEL >= 0)
-#define TRACE0(x)           TRACE(x)
+#if defined(TRACELEVEL) && (TRACELEVEL >= 0)
+#    define TRACE0(x) TRACE(x)
 #else
-#define TRACE0(x)
+#    define TRACE0(x)
 #endif
 
 #if TRACELEVEL > 0
-#define TRACE1(x)           TRACE(x)
+#    define TRACE1(x) TRACE(x)
 #else
-#define TRACE1(x)
+#    define TRACE1(x)
 #endif
 
 #if TRACELEVEL > 1
-#define TRACE2(x)           TRACE(x)
+#    define TRACE2(x) TRACE(x)
 #else
-#define TRACE2(x)
+#    define TRACE2(x)
 #endif
 
 #if TRACELEVEL > 2
-#define TRACE3(x)           TRACE(x)
+#    define TRACE3(x) TRACE(x)
 #else
-#define TRACE3(x)
+#    define TRACE3(x)
 #endif
 
 #if TRACELEVEL > 3
-#define TRACE4(x)           TRACE(x)
+#    define TRACE4(x) TRACE(x)
 #else
-#define TRACE4(x)
+#    define TRACE4(x)
 #endif
 
 #if TRACELEVEL > 4
-#define TRACE5(x)           TRACE(x)
+#    define TRACE5(x) TRACE(x)
 #else
-#define TRACE5(x)
+#    define TRACE5(x)
 #endif
 
 #if TRACELEVEL > 5
-#define TRACE6(x)           TRACE(x)
+#    define TRACE6(x) TRACE(x)
 #else
-#define TRACE6(x)
+#    define TRACE6(x)
 #endif
 
 #if TRACELEVEL > 6
-#define TRACE7(x)           TRACE(x)
+#    define TRACE7(x) TRACE(x)
 #else
-#define TRACE7(x)
+#    define TRACE7(x)
 #endif
 
 #if TRACELEVEL > 7
-#define TRACE8(x)           TRACE(x)
+#    define TRACE8(x) TRACE(x)
 #else
-#define TRACE8(x)
+#    define TRACE8(x)
 #endif
 
 #if TRACELEVEL > 8
-#define TRACE9(x)           TRACE(x)
+#    define TRACE9(x) TRACE(x)
 #else
-#define TRACE9(x)
+#    define TRACE9(x)
 #endif
 
 #endif
