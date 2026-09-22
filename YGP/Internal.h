855046c0b7e0643788844134592ffb0d2bf7a9d1
Markus Schwab <markus.schwab.32@gmail.com>
2026-09-22 17:29:33 +0200

    YGP/Internal: Re-formatted

--- YGP/Internal.h
+++ YGP/Internal.h
@@ -1,8 +1,6 @@
 #ifndef YGP_INTERNAL_H
 #define YGP_INTERNAL_H
 
-//$Id: Internal.h,v 1.11 2008/05/18 13:21:27 markus Rel $
-
 // This file is part of libYGP.
 //
 // libYGP is free software: you can redistribute it and/or modify
@@ -18,25 +16,23 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include <ygp-cfg.h>
 
-
-#if defined (HAVE_GETTEXT) && defined (ENABLE_NLS)
-#  include <libintl.h>
-#  ifdef CONVERT_TO_UTF8
-#     include <glibmm/convert.h>
-#     define _(String)                        Glib::locale_to_utf8 (dgettext (LIBYGP_NAME, String))
-#  else
-#     define _(String)                        dgettext (LIBYGP_NAME, String)
-#  endif
-#  define N_(String)                          (String)
+#if defined(HAVE_GETTEXT) && defined(ENABLE_NLS)
+#    include <libintl.h>
+#    ifdef CONVERT_TO_UTF8
+#        include <glibmm/convert.h>
+#        define _(String) Glib::locale_to_utf8(dgettext(LIBYGP_NAME, String))
+#    else
+#        define _(String) dgettext(LIBYGP_NAME, String)
+#    endif
+#    define N_(String) (String)
 #else
-#  define _(String)                           (String)
-#  define N_(String)                          (String)
-#  define textdomain(domain)
-#  define binddomain(Domain)
-#  define bindtextdomain(Package, Directory)
+#    define _(String) (String)
+#    define N_(String) (String)
+#    define textdomain(domain)
+#    define binddomain(Domain)
+#    define bindtextdomain(Package, Directory)
 #endif
 
 #endif
