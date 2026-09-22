26a6871f3edff45b684d0b0347179b1e4aa4370d
Markus Schwab <markus.schwab.32@gmail.com>
2026-09-22 15:01:53 +0200

    YGP/DirSrch: Re-formatted

--- YGP/DirSrch.cpp
+++ YGP/DirSrch.cpp
@@ -1,14 +1,11 @@
-//$Id: DirSrch.cpp,v 1.56 2008/03/29 17:35:17 markus Rel $
-
-//PROJECT     : libYGP
-//SUBSYSTEM   : DirSrch
-//REFERENCES  :
-//TODO        :
-//BUGS        :
-//REVISION    : $Revision: 1.56 $
-//AUTHOR      : Markus Schwab
-//CREATED     : 22.7.1999
-//COPYRIGHT   : Copyright (C) 1999 - 2005, 2007, 2008
+// PROJECT     : libYGP
+// SUBSYSTEM   : DirSrch
+// REFERENCES  :
+// TODO        :
+// BUGS        :
+// AUTHOR      : Markus Schwab
+// CREATED     : 22.7.1999
+// COPYRIGHT   : Copyright (C) 1999 - 2005, 2007, 2008, 2026
 
 // This file is part of libYGP.
 //
@@ -25,41 +22,41 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include <ygp-cfg.h>
 
 #if SYSTEM == UNIX
-#  include <unistd.h>
+#    include <unistd.h>
 #elif SYSTEM == WINDOWS
-#  include <sys/types.h>
-#  include <sys/stat.h>
+#    include <sys/stat.h>
+#    include <sys/types.h>
 #endif
 
 #include <cerrno>
 
-#include "YGP/File.h"
 #include "YGP/Check.h"
-#include "YGP/Trace.h"
+#include "YGP/File.h"
 #include "YGP/FileRExp.h"
+#include "YGP/Trace.h"
 
 #include "YGP/DirSrch.h"
-
 
 namespace YGP {
 
 //-----------------------------------------------------------------------------
 /// Defaultconstructor
 //-----------------------------------------------------------------------------
-DirectorySearch::DirectorySearch () : IDirectorySearch (), searchDir (1, '.'),
-				      searchFile (), attr (0), offStrip (0)
+DirectorySearch::DirectorySearch()
+    : IDirectorySearch(), searchDir(1, '.'), searchFile(), attr(0), offStrip(0)
 #if SYSTEM == UNIX
-     , pDir (NULL)
+      ,
+      pDir(NULL)
 #else
-     , hSearch (INVALID_HANDLE_VALUE)
+      ,
+      hSearch(INVALID_HANDLE_VALUE)
 #endif
 {
-   TRACE9 ("DirectorySearch::DirectorySearch ()");
-   searchDir += File::DIRSEPARATOR;
+    TRACE9("DirectorySearch::DirectorySearch()");
+    searchDir += File::DIRSEPARATOR;
 }
 
 //-----------------------------------------------------------------------------
@@ -67,25 +64,22 @@ DirectorySearch::DirectorySearch () : IDirectorySearch (), searchDir (1, '.'),
 /// include path-information.
 /// \param search Files (including the path) to search for.
 //-----------------------------------------------------------------------------
-DirectorySearch::DirectorySearch (const std::string& search)
-   : IDirectorySearch (), searchDir (), searchFile (), attr (0), offStrip (0),
+DirectorySearch::DirectorySearch(const std::string& search)
+    : IDirectorySearch(), searchDir(), searchFile(), attr(0), offStrip(0),
 #if SYSTEM == UNIX
-     pDir (NULL)
+      pDir(NULL)
 #else
-     hSearch (INVALID_HANDLE_VALUE)
+      hSearch(INVALID_HANDLE_VALUE)
 #endif
 {
-   TRACE9 ("DirectorySearch::DirectorySearch (const std::string&) - " << search.c_str ());
-   setSearchValue (search);
+    TRACE9("DirectorySearch::DirectorySearch(const std::string&) - " << search.c_str());
+    setSearchValue(search);
 }
 
 //-----------------------------------------------------------------------------
 /// Destructor
 //-----------------------------------------------------------------------------
-DirectorySearch::~DirectorySearch () {
-   cleanup ();
-}
-
+DirectorySearch::~DirectorySearch() { cleanup(); }
 
 //-----------------------------------------------------------------------------
 /// Retrieves the first file matching the previously specified
@@ -94,61 +88,56 @@ DirectorySearch::~DirectorySearch () {
 /// \returns const File* Pointer to found file-object or NULL
 /// \pre The search values must have been specified!
 //-----------------------------------------------------------------------------
-const File* DirectorySearch::find (unsigned long attribs) {
-   TRACE9 ("DirectorySearch::find (unsigned long)");
-   cleanup ();
+const File* DirectorySearch::find(unsigned long attribs) {
+    TRACE9("DirectorySearch::find(unsigned long)");
+    cleanup();
 
-   attr = attribs;
+    attr = attribs;
 
-   TRACE5 ("DirectorySearch::find (unsigned long) " << searchDir.c_str ()
-	   << searchFile.c_str ());
+    TRACE5("DirectorySearch::find(unsigned long) " << searchDir.c_str() << searchFile.c_str());
 
-   pEntry = new File;
-   // Set path-information; but cut path in case of special cases
-   pEntry->path_ = offStrip
-      ? searchDir.substr ((offStrip > 0) ? offStrip : ((offStrip < 0) ? -offStrip : 0))
-      : searchDir;
-   TRACE8 ("DirectorySearch::find (unsigned long) - Search in dir " << pEntry->path_);
-   Check3 (!checkIntegrity ());
+    pEntry = new File;
+    // Set path-information; but cut path in case of special cases
+    pEntry->path_ = offStrip ? searchDir.substr((offStrip > 0) ? offStrip : ((offStrip < 0) ? -offStrip : 0)) : searchDir;
+    TRACE8("DirectorySearch::find(unsigned long) - Search in dir " << pEntry->path_);
+    Check3(!checkIntegrity());
 
 #if SYSTEM == UNIX
-   pDir = opendir (searchDir.c_str ());
-   TRACE9 ("DirectorySearch::find (unsigned long) - opendir: " << (pDir ? "OK" : "Failed"));
-   if (!pDir) {
-      clearEntry ();
-      return NULL;
-   }
+    pDir = opendir(searchDir.c_str());
+    TRACE9("DirectorySearch::find(unsigned long) - opendir: " << (pDir ? "OK" : "Failed"));
+    if (!pDir) {
+        clearEntry();
+        return NULL;
+    }
 #else
-#  if SYSTEM == WINDOWS
-   std::string temp (searchDir + '*');
-   hSearch = FindFirstFile (temp.c_str (), pEntry);
-   if (hSearch == INVALID_HANDLE_VALUE) {
-      clearEntry ();
-      return NULL;
-   }
-#  else
-#     error Not implemented!
-#  endif
+#    if SYSTEM == WINDOWS
+    std::string temp(searchDir + '*');
+    hSearch = FindFirstFile(temp.c_str(), pEntry);
+    if (hSearch == INVALID_HANDLE_VALUE) {
+        clearEntry();
+        return NULL;
+    }
+#    else
+#        error Not implemented!
+#    endif
 #endif
 
 #if SYSTEM == UNIX
-   return next ();
+    return next();
 #else
-#  if SYSTEM == WINDOWS
-   TRACE8 ("DirectorySearch::find (unsigned long) - found " << pEntry->name ());
+#    if SYSTEM == WINDOWS
+    TRACE8("DirectorySearch::find(unsigned long) - found " << pEntry->name());
 
-   FileRegularExpr regExp (searchFile.c_str ());
-   Check3 (!regExp.checkIntegrity ());
+    FileRegularExpr regExp(searchFile.c_str());
+    Check3(!regExp.checkIntegrity());
 
-   // Attribut-handling: Files having attrs not specified here are not returned
-   unsigned long attr_ = ~convertToSysAttribs (attr | FILE_ATTRIBUTE_ARCHIVE);
+    // Attribut-handling: Files having attrs not specified here are not returned
+    unsigned long attr_ = ~convertToSysAttribs(attr | FILE_ATTRIBUTE_ARCHIVE);
 
-   return ((pEntry->attributes () & attr_)
-           || !regExp.matches (pEntry->name ()))
-          ? next () : pEntry;
-#  else
-#     error Not implemented!
-#  endif
+    return ((pEntry->attributes() & attr_) || !regExp.matches(pEntry->name())) ? next() : pEntry;
+#    else
+#        error Not implemented!
+#    endif
 #endif
 }
 
@@ -158,64 +147,62 @@ const File* DirectorySearch::find (unsigned long attribs) {
 /// \returns const File* Pointer to found file-object or NULL
 /// \pre The search must have already been started by a find!
 //-----------------------------------------------------------------------------
-const File* DirectorySearch::next () {
-   TRACE9 ("DirectorySearch::next ()");
+const File* DirectorySearch::next() {
+    TRACE9("DirectorySearch::next()");
 
-   Check3 (!checkIntegrity ());
-   FileRegularExpr regExp (searchFile.c_str ());
-   Check3 (!regExp.checkIntegrity ());
+    Check3(!checkIntegrity());
+    FileRegularExpr regExp(searchFile.c_str());
+    Check3(!regExp.checkIntegrity());
 
 #if SYSTEM == UNIX
-   Check3 (pDir);
+    Check3(pDir);
 
-   unsigned long attr_ = convertToSysAttribs (attr);
+    unsigned long attr_ = convertToSysAttribs(attr);
 
-   std::string workfile (pEntry->path_);
-   std::string temp;
-   struct dirent* pDirEnt;
-   while ((pDirEnt = readdir (pDir)) != NULL) {            // Files available?
-      TRACE8 ("DirectorySearch::next () - found " << pDirEnt->d_name);
+    std::string workfile(pEntry->path_);
+    std::string temp;
+    struct dirent* pDirEnt;
+    while ((pDirEnt = readdir(pDir)) != NULL) { // Files available?
+        TRACE8("DirectorySearch::next() - found " << pDirEnt->d_name);
 
-      if ((!(attr & FILE_HIDDEN)) && (*pDirEnt->d_name == '.')
-          && searchFile[0] != '.')
-         continue;
+        if ((!(attr & FILE_HIDDEN)) && (*pDirEnt->d_name == '.') && searchFile[0] != '.')
+            continue;
 
-      if (regExp.matches (pDirEnt->d_name)) {
-	 temp = workfile + pDirEnt->d_name;
+        if (regExp.matches(pDirEnt->d_name)) {
+            temp = workfile + pDirEnt->d_name;
 
-	 if (!stat (temp.c_str (), &pEntry->status)) {
-            // Do attributes match?
-            TRACE9 ("DirectorySearch::next (): " << pDirEnt->d_name << " (" << std::hex
-                    << attr_ << ") -> Mode: " << pEntry->status.st_mode << std::dec);
-            if ((attr_ & pEntry->status.st_mode) == pEntry->status.st_mode) {
-               pEntry->entry = *pDirEnt;
-               pEntry->userExec = !access (temp.c_str (), X_OK);
-               TRACE1 ("DirectorySearch::next () - match " << pEntry->name ());
-               return pEntry;
-            } // endif attributs OK
-	 } // endif stat succeeded
-      } // endif filename OK
-   } // end-while files available
+            if (!stat(temp.c_str(), &pEntry->status)) {
+                // Do attributes match?
+                TRACE9("DirectorySearch::next(): " << pDirEnt->d_name << " (" << std::hex << attr_ << ") -> Mode: "
+		       << pEntry->status.st_mode << std::dec);
+                if ((attr_ & pEntry->status.st_mode) == pEntry->status.st_mode) {
+                    pEntry->entry = *pDirEnt;
+                    pEntry->userExec = !access(temp.c_str(), X_OK);
+                    TRACE1("DirectorySearch::next() - match " << pEntry->name());
+                    return pEntry;
+                } // endif attributs OK
+            } // endif stat succeeded
+        } // endif filename OK
+    } // end-while files available
 
-   clearEntry ();
-   return NULL;
+    clearEntry();
+    return NULL;
 #else
-#  if SYSTEM == WINDOWS
-   // Attribut-handling: Files having attrs not specified here are not returned
-   unsigned long attr_ = ~convertToSysAttribs (attr | FILE_ATTRIBUTE_ARCHIVE);
+#    if SYSTEM == WINDOWS
+    // Attribut-handling: Files having attrs not specified here are not returned
+    unsigned long attr_ = ~convertToSysAttribs(attr | FILE_ATTRIBUTE_ARCHIVE);
 
-   while (FindNextFile (hSearch, pEntry))
-      if (!(pEntry->attributes () & attr_)
-          && regExp.matches (pEntry->name ())) {
-         TRACE1 ("DirectorySearch::find () - match " << pEntry->name ());
-         return pEntry;
-      }
+    while (FindNextFile(hSearch, pEntry))
+        if (!(pEntry->attributes() & attr_) && regExp.matches(pEntry->name())) {
+            TRACE1("DirectorySearch::find() - match " << pEntry->name());
+            return pEntry;
+        }
 
-   clearEntry ();
-   return NULL;
-#  else
-#      error Not implemented yet!
-#   endif
+    clearEntry();
+    return NULL;
+#    else
+#        error Not implemented yet!
+#    endif
 #endif
 }
 
@@ -224,16 +211,12 @@ const File* DirectorySearch::next () {
 /// describing the error.
 /// \returns int Status; 0: OK
 //-----------------------------------------------------------------------------
-int DirectorySearch::checkIntegrity () const {
-   TRACE9 ("DirectorySearch::checkIntegrity () const");
+int DirectorySearch::checkIntegrity() const {
+    TRACE9("DirectorySearch::checkIntegrity() const");
 
-   return (searchDir.empty ()
-	   ? NO_DIR
-	   : (searchFile.empty ()
-	      ? NO_FILE
-	      : (pEntry
-		 ? DIRSRCH_OK + (pEntry->path_.empty () && !offStrip)
-		 : NO_ENTRY)));
+    return (searchDir.empty()
+                ? NO_DIR
+                : (searchFile.empty() ? NO_FILE : (pEntry ? DIRSRCH_OK + (pEntry->path_.empty() && !offStrip) : NO_ENTRY)));
 }
 
 //-----------------------------------------------------------------------------
@@ -242,69 +225,70 @@ int DirectorySearch::checkIntegrity () const {
 /// new search.
 /// \param search Files to find
 //-----------------------------------------------------------------------------
-void DirectorySearch::setSearchValue (const std::string& search) {
-   TRACE8 ("DirectorySearch::setSearchValue (const std::string&) - " << search);
-   Check3 (!search.empty ());
+void DirectorySearch::setSearchValue(const std::string& search) {
+    TRACE8("DirectorySearch::setSearchValue(const std::string&) - " << search);
+    Check3(!search.empty());
 
-   pEntry = NULL;          // New search-value means new search with new result
-   offStrip = 0;
+    pEntry = NULL; // New search-value means new search with new result
+    offStrip = 0;
 
-   searchFile = search;
+    searchFile = search;
 
-   size_t len (search.length () - 1);
-   if (searchFile[len] == File::DIRSEPARATOR)
-      searchFile.replace (len, 1, 0, '\0');
+    size_t len(search.length() - 1);
+    if (searchFile[len] == File::DIRSEPARATOR)
+        searchFile.replace(len, 1, 0, '\0');
 
-   len = searchFile.rfind (File::DIRSEPARATOR);
-   if (len != std::string::npos) {
-      searchDir = searchFile;
-      TRACE9 ("DirectorySearch::setSearchValue - 1: " << searchDir);
-      searchDir.replace (len + 1, searchDir.length (), 0, '\0');
-      TRACE9 ("DirectorySearch::setSearchValue - 2: " << searchDir);
-      searchFile.replace (0, len + 1, 0, '\0');
-      TRACE9 ("DirectorySearch::setSearchValue - 3: " << searchFile);
-   }
-   else {
-      if (searchFile.empty ()) {
-         searchDir = File::DIRSEPARATOR;
-         searchFile = '.';
-         offStrip = 1;
-      }
-      else {
-         offStrip = 2;
-         searchDir = '.';
-         searchDir += File::DIRSEPARATOR;
-      }
-   }
-   TRACE9 ("DirectorySearch::setSearchValue - checkIntegrity () = " << checkIntegrity ());
-   Check3 (checkIntegrity () <= NO_ENTRY);
+    len = searchFile.rfind(File::DIRSEPARATOR);
+    if (len != std::string::npos) {
+        searchDir = searchFile;
+        TRACE9("DirectorySearch::setSearchValue - 1: " << searchDir);
+        searchDir.replace(len + 1, searchDir.length(), 0, '\0');
+        TRACE9("DirectorySearch::setSearchValue - 2: " << searchDir);
+        searchFile.replace(0, len + 1, 0, '\0');
+        TRACE9("DirectorySearch::setSearchValue - 3: " << searchFile);
+    }
+    else {
+        if (searchFile.empty()) {
+            searchDir = File::DIRSEPARATOR;
+            searchFile = '.';
+            offStrip = 1;
+        }
+        else {
+            offStrip = 2;
+            searchDir = '.';
+            searchDir += File::DIRSEPARATOR;
+        }
+    }
+    TRACE9("DirectorySearch::setSearchValue - checkIntegrity() = " << checkIntegrity());
+    Check3(checkIntegrity() <= NO_ENTRY);
 }
 
 //-----------------------------------------------------------------------------
 /// Releases all internally used resources.
 //-----------------------------------------------------------------------------
-void DirectorySearch::cleanup () {
-   TRACE9 ("DirectorySearch::cleanup ()");
-   clearEntry ();
+void DirectorySearch::cleanup() {
+    TRACE9("DirectorySearch::cleanup()");
+    clearEntry();
 
 #if SYSTEM == UNIX
-#  ifdef CLOSEDIR_VOID
-   if (pDir)
-      closedir (pDir);
-#  else
-   if (pDir) {
-#if CHECK >= 3
-      int rc =
-#endif
-      closedir (pDir);
-      Check3 (!rc); }
-#  endif
-   pDir = NULL;
+#    ifdef CLOSEDIR_VOID
+    if (pDir)
+        closedir(pDir);
+#    else
+    if (pDir) {
+#        if CHECK >= 3
+        int rc =
+#        endif
+            closedir(pDir);
+        Check3(!rc);
+    }
+#    endif
+    pDir = NULL;
 #elif SYSTEM == WINDOWS
-   FindClose (hSearch);
-   hSearch = INVALID_HANDLE_VALUE;
+    FindClose(hSearch);
+    hSearch = INVALID_HANDLE_VALUE;
 #else
-#  error Not implemented yet!
+#    error Not implemented yet!
 #endif
 }
 
@@ -313,18 +297,18 @@ void DirectorySearch::cleanup () {
 /// \param dir Directory whose validity should be checked
 /// \returns bool True if the directory exists
 //-----------------------------------------------------------------------------
-bool DirectorySearch::isValid (const std::string& dir) {
-   TRACE8 ("DirectorySearch::isValid (const std::string&) - " << dir.c_str ());
+bool DirectorySearch::isValid(const std::string& dir) {
+    TRACE8("DirectorySearch::isValid(const std::string&) - " << dir.c_str());
 
-   struct stat file;
+    struct stat file;
 
 #ifdef _MSC_VER
-   std::string temp (dir);
-   if (temp[temp.length () - 1] == File::DIRSEPARATOR)
-      temp.replace (temp.length () - 1, 1, 0, '\0');
-   return (!::stat (temp.c_str (), &file) && (file.st_mode & _S_IFDIR));
+    std::string temp(dir);
+    if (temp[temp.length() - 1] == File::DIRSEPARATOR)
+        temp.replace(temp.length() - 1, 1, 0, '\0');
+    return (!::stat(temp.c_str(), &file) && (file.st_mode & _S_IFDIR));
 #else
-   return (!::stat (dir.c_str (), &file) && (file.st_mode & S_IFDIR));
+    return (!::stat(dir.c_str(), &file) && (file.st_mode & S_IFDIR));
 #endif
 }
 
@@ -333,8 +317,6 @@ bool DirectorySearch::isValid (const std::string& dir) {
 /// directory.
 /// \returns bool True if the directory exists
 //-----------------------------------------------------------------------------
-bool DirectorySearch::isValid () const {
-   return isValid (searchDir);
-}
+bool DirectorySearch::isValid() const { return isValid(searchDir); }
 
-}
+} // namespace YGP
--- YGP/DirSrch.h
+++ YGP/DirSrch.h
@@ -1,8 +1,6 @@
 #ifndef YGP_DIRSRCH_H
 #define YGP_DIRSRCH_H
 
-//$Id: DirSrch.h,v 1.39 2008/05/18 13:21:27 markus Rel $
-
 // This file is part of libYGP.
 //
 // libYGP is free software: you can redistribute it and/or modify
@@ -18,31 +16,30 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
-#include <ctime>
 #include <cstddef>
 #include <cstring>
+#include <ctime>
 
 #if SYSTEM == UNIX
-#  if HAVE_DIRENT_H
-#     include <dirent.h>
-#     undef NAMLEN
-#     define NAMLEN(dirent) strlen((dirent)->d_name)
-#  else
-#     undef dirent
-#     undef NAMLEN
-#     define dirent direct
-#     define NAMLEN(dirent) (dirent)->d_namlen
-#     if HAVE_SYS_NDIR_H
-#        include <sys/ndir.h>
-#     endif
-#     if HAVE_SYS_DIR_H
-#        include <sys/dir.h>
-#     endif
-#     if HAVE_NDIR_H
-#        include <ndir.h>
-#     endif
-#  endif
+#    if HAVE_DIRENT_H
+#        include <dirent.h>
+#        undef NAMLEN
+#        define NAMLEN(dirent) strlen((dirent)->d_name)
+#    else
+#        undef dirent
+#        undef NAMLEN
+#        define dirent direct
+#        define NAMLEN(dirent) (dirent)->d_namlen
+#        if HAVE_SYS_NDIR_H
+#            include <sys/ndir.h>
+#        endif
+#        if HAVE_SYS_DIR_H
+#            include <sys/dir.h>
+#        endif
+#        if HAVE_NDIR_H
+#            include <ndir.h>
+#        endif
+#    endif
 #endif
 
 #include <ygp-cfg.h>
@@ -52,7 +49,6 @@
 namespace YGP {
 
 struct File;
-
 
 /**Class to search for files in a certain (locally accesable) directory. This
    search can be restricted to files matching certain name-criterias or by
@@ -71,70 +67,72 @@ struct File;
          (like expanding the tilde (~) to the home-directory)!
 */
 class DirectorySearch : public IDirectorySearch {
- public:
-   //@Section manager-functions
-   DirectorySearch ();
-   DirectorySearch (const std::string& search);
-   virtual ~DirectorySearch ();
+  public:
+    //@Section manager-functions
+    DirectorySearch();
+    DirectorySearch(const std::string& search);
+    virtual ~DirectorySearch();
 
-   virtual void setSearchValue (const std::string& search);
-   /// Returns the directory part of the files to search for.
-   virtual std::string getDirectory () const { return searchDir; }
-   /// Returns the file specification of the files to search for.
-   virtual std::string getFileSpec () const { return searchFile; }
+    virtual void setSearchValue(const std::string& search);
+    /// Returns the directory part of the files to search for.
+    virtual std::string getDirectory() const { return searchDir; }
+    /// Returns the file specification of the files to search for.
+    virtual std::string getFileSpec() const { return searchFile; }
 
-   virtual bool isValid () const;
-   static bool isValid (const std::string& dir);
+    virtual bool isValid() const;
+    static bool isValid(const std::string& dir);
 
-   /// \name Searching
-   //@{
-   /// Searches for the specified files with the passed attributes.
-   /// \param search Files to search for
-   /// \param attribs Attributes the searched files must have
-   /// \returns const File* Pointer to found file or NULL
-   const File* find (const std::string& search,
-                     unsigned long attribs = IDirectorySearch::FILE_NORMAL) {
-      cleanup ();
-      setSearchValue (search);
-      return find (attribs); }
-   /// Searches for previously specified files with the passed attributes.
-   /// \param attribs Attributes the searched files must have
-   /// \returns const File* Pointer to found file or NULL
-   virtual const File* find (unsigned long attribs = IDirectorySearch::FILE_NORMAL);
-   /// Method to find the next file matching the  previously specified values.
-   /// \returns const File* Pointer to found file or NULL
-   virtual const File* next ();
-   //@}
+    /// \name Searching
+    //@{
+    /// Searches for the specified files with the passed attributes.
+    /// \param search Files to search for
+    /// \param attribs Attributes the searched files must have
+    /// \returns const File* Pointer to found file or NULL
+    const File* find(const std::string& search, unsigned long attribs = IDirectorySearch::FILE_NORMAL) {
+        cleanup();
+        setSearchValue(search);
+        return find(attribs);
+    }
+    /// Searches for previously specified files with the passed attributes.
+    /// \param attribs Attributes the searched files must have
+    /// \returns const File* Pointer to found file or NULL
+    virtual const File* find(unsigned long attribs = IDirectorySearch::FILE_NORMAL);
+    /// Method to find the next file matching the  previously specified values.
+    /// \returns const File* Pointer to found file or NULL
+    virtual const File* next();
+    //@}
 
- protected:
-   virtual int checkIntegrity () const;
+  protected:
+    virtual int checkIntegrity() const;
 
-   void cleanup ();
+    void cleanup();
 
-   std::string searchDir;   ///< Holding the directory part of the search value
-   std::string searchFile;       ///< Holding the file part of the search value
+    std::string searchDir;  ///< Holding the directory part of the search value
+    std::string searchFile; ///< Holding the file part of the search value
 
-   unsigned long attr;             ///< Attributes the found file must have set
-   /// Status of object
-   enum { DIRSRCH_OK = 0,                       ///< Object is in a valid state
-          NO_ENTRY,     ///< Object is in a valid state, but has no entry found
-          NO_DIR,                      ///< No directory to search in specified
-          NO_FILE,                         ///< No file to search for specified
-          LAST };
+    unsigned long attr; ///< Attributes the found file must have set
+    /// Status of object
+    enum {
+        DIRSRCH_OK = 0, ///< Object is in a valid state
+        NO_ENTRY,       ///< Object is in a valid state, but has no entry found
+        NO_DIR,         ///< No directory to search in specified
+        NO_FILE,        ///< No file to search for specified
+        LAST
+    };
 
- private:
-   //@Section prohibited manager functions
-   DirectorySearch (const DirectorySearch&);
-   DirectorySearch& operator= (const DirectorySearch&);
+  private:
+    //@Section prohibited manager functions
+    DirectorySearch(const DirectorySearch&);
+    DirectorySearch& operator=(const DirectorySearch&);
 
-   int offStrip;
+    int offStrip;
 #if SYSTEM == UNIX
-   DIR* pDir;
+    DIR* pDir;
 #elif SYSTEM == WINDOWS
-   HANDLE hSearch;
+    HANDLE hSearch;
 #endif
 };
 
-}
+} // namespace YGP
 
 #endif // DIRSRCH_H
