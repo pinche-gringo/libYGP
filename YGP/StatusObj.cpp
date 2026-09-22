727fbb771c9d034f819591327c69064460a24253
Markus Schwab <markus.schwab.32@gmail.com>
2026-09-22 18:04:47 +0200

    YGP/StatusObj: Re-formatted

--- YGP/StatusObj.cpp
+++ YGP/StatusObj.cpp
@@ -1,14 +1,11 @@
-//$Id: StatusObj.cpp,v 1.8 2008/03/29 17:35:17 markus Rel $
-
-//PROJECT     : libYGP
-//SUBSYSTEM   : YGP/StatusObject
-//REFERENCES  :
-//TODO        :
-//BUGS        :
-//REVISION    : $Revision: 1.8 $
-//AUTHOR      : Markus Schwab
-//CREATED     : 19.11.2003
-//COPYRIGHT   : Copyright (C) 2003, 2004, 2008, 2009
+// PROJECT     : libYGP
+// SUBSYSTEM   : YGP/StatusObject
+// REFERENCES  :
+// TODO        :
+// BUGS        :
+// AUTHOR      : Markus Schwab
+// CREATED     : 19.11.2003
+// COPYRIGHT   : Copyright (C) 2003, 2004, 2008, 2009, 2026
 
 // This file is part of libYGP.
 //
@@ -25,55 +22,46 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include <YGP/StatusObj.h>
-
 
 namespace YGP {
 
 //-----------------------------------------------------------------------------
 /// Default constructor
 //-----------------------------------------------------------------------------
-StatusObject::StatusObject () : tp (UNDEFINED), msg (), child () {
-}
+StatusObject::StatusObject() : tp(UNDEFINED), msg(), child() {}
 
 //-----------------------------------------------------------------------------
 /// Constructor from a type and a message
 /// \param t Type of the object
 /// \param message Message of the object
 //-----------------------------------------------------------------------------
-StatusObject::StatusObject (type t, const std::string& message)
-   : tp (t), msg (message), child () {
-}
+StatusObject::StatusObject(type t, const std::string& message) : tp(t), msg(message), child() {}
 
 //-----------------------------------------------------------------------------
 /// Copy constructor
 /// \param other Object to copy
 //-----------------------------------------------------------------------------
-StatusObject::StatusObject (const StatusObject& other)
-   : tp (other.tp), msg (other.msg),
-     child (other.child ? new StatusObject (*other.child) : NULL) {
-}
+StatusObject::StatusObject(const StatusObject& other)
+    : tp(other.tp), msg(other.msg), child(other.child ? new StatusObject(*other.child) : NULL) {}
 
 //-----------------------------------------------------------------------------
 /// Destructor
 //-----------------------------------------------------------------------------
-StatusObject::~StatusObject () {
-}
-
+StatusObject::~StatusObject() {}
 
 //-----------------------------------------------------------------------------
 /// Assignment operator
 /// \param other Object to copy the values from
 //-----------------------------------------------------------------------------
-StatusObject& StatusObject::operator= (const StatusObject& other) {
-   if (&other != this) {
-      tp = other.tp;
-      msg = other.msg;
-      child.reset (other.child ? new StatusObject (*other.child) : NULL);
-   }
+StatusObject& StatusObject::operator=(const StatusObject& other) {
+    if (&other != this) {
+        tp = other.tp;
+        msg = other.msg;
+        child.reset(other.child ? new StatusObject(*other.child) : NULL);
+    }
 
-   return *this;
+    return *this;
 }
 
 //----------------------------------------------------------------------------
@@ -83,10 +71,10 @@ StatusObject& StatusObject::operator= (const StatusObject& other) {
 /// \remarks - The message is appended to existing ones
 ///     - If the passed type is more important than the old one, it is changed
 //----------------------------------------------------------------------------
-void StatusObject::setMessage (type t, const std::string& message) {
-   msg += message;
-   if (t > tp)
-      tp = t;
+void StatusObject::setMessage(type t, const std::string& message) {
+    msg += message;
+    if (t > tp)
+        tp = t;
 }
 
 //----------------------------------------------------------------------------
@@ -95,13 +83,13 @@ void StatusObject::setMessage (type t, const std::string& message) {
 /// \remarks - The message replaces existing ones
 ///     - The old messages are accessable as "details"
 //----------------------------------------------------------------------------
-void StatusObject::generalize (const std::string& message) {
+void StatusObject::generalize(const std::string& message) {
     if (child)
-       child->msg += msg;
+        child->msg += msg;
     else
-       child.reset (new StatusObject (tp, msg));
+        child.reset(new StatusObject(tp, msg));
 
     msg = message;
 }
 
-}
+} // namespace YGP
--- YGP/StatusObj.h
+++ YGP/StatusObj.h
@@ -1,8 +1,6 @@
 #ifndef YGP_STATUSOBJ_H
 #define YGP_STATUSOBJ_H
 
-//$Id: StatusObj.h,v 1.6 2008/05/18 18:48:24 markus Rel $
-
 // This file is part of libYGP.
 //
 // libYGP is free software: you can redistribute it and/or modify
@@ -18,11 +16,9 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include <string>
 
 #include <boost/scoped_ptr.hpp>
-
 
 namespace YGP {
 
@@ -33,46 +29,51 @@ namespace YGP {
    information as sort of details).
 */
 class StatusObject {
- public:
-   /// The type of the object
-   typedef enum { UNDEFINED,               ///< The object contains no message
-                  INFO,      ///< The object contains an informational message
-                  WARNING,          ///< The object contains a warning message
-                  ERROR                      ///< The object contains an error
-   } type;
+  public:
+    /// The type of the object
+    typedef enum {
+        UNDEFINED, ///< The object contains no message
+        INFO,      ///< The object contains an informational message
+        WARNING,   ///< The object contains a warning message
+        ERROR      ///< The object contains an error
+    } type;
 
-   StatusObject ();
-   StatusObject (type t, const std::string& message);
-   StatusObject (const StatusObject& other);
-   virtual ~StatusObject ();
+    StatusObject();
+    StatusObject(type t, const std::string& message);
+    StatusObject(const StatusObject& other);
+    virtual ~StatusObject();
 
-   StatusObject& operator= (const StatusObject& other);
+    StatusObject& operator=(const StatusObject& other);
 
-   /// Returns the type of the message
-   type getType () const { return tp; }
+    /// Returns the type of the message
+    type getType() const { return tp; }
 
-   /// Returns the message
-   std::string getMessage () const { return msg; }
+    /// Returns the message
+    std::string getMessage() const { return msg; }
 
-   /// Returns the details of the message (after the object has been abstracted)
-   std::string getDetails () const { return child ? child->msg : ""; }
+    /// Returns the details of the message (after the object has been abstracted)
+    std::string getDetails() const { return child ? child->msg : ""; }
 
-   /// Returns if the message has details
-   bool hasDetails () const { return (bool)child; }
+    /// Returns if the message has details
+    bool hasDetails() const { return (bool)child; }
 
-   /// Cleans the object (sets it again to undefined)
-   void clean () { tp = UNDEFINED; msg.clear (); child.reset (NULL); }
+    /// Cleans the object (sets it again to undefined)
+    void clean() {
+        tp = UNDEFINED;
+        msg.clear();
+        child.reset(NULL);
+    }
 
-   void generalize (const std::string& message);
-   void setMessage (type t, const std::string& message);
+    void generalize(const std::string& message);
+    void setMessage(type t, const std::string& message);
 
- private:
-   type        tp;
-   std::string msg;
+  private:
+    type tp;
+    std::string msg;
 
-   boost::scoped_ptr<StatusObject> child;
+    boost::scoped_ptr<StatusObject> child;
 };
 
-}
+} // namespace YGP
 
 #endif
