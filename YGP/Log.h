646826c0ebafb27db85ef3e7eeeb1cacd988f0c9
Markus Schwab <markus.schwab.32@gmail.com>
2026-09-22 17:31:29 +0200

    YGP/Log: Re-formatted

--- YGP/Log.h
+++ YGP/Log.h
@@ -1,8 +1,6 @@
 #ifndef YGP_LOG_H
 #define YGP_LOG_H
 
-//$Id: Log.h,v 1.22 2008/05/18 18:47:59 markus Rel $
-
 // This file is part of libYGP.
 //
 // libYGP is free software: you can redistribute it and/or modify
@@ -18,32 +16,29 @@
 // You should have received a copy of the GNU General Public License
 // along with libYGP.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #include <ygp-cfg.h>
 
 #ifdef HAVE_SYSLOG_H
-#  include <syslog.h>
+#    include <syslog.h>
 #else
-#  include <iostream>
-#  include <map>
+#    include <iostream>
+#    include <map>
 
-#  include <YGP/Process.h>
+#    include <YGP/Process.h>
 #endif
 
 #include <YGP/Check.h>
 
-
 namespace YGP {
 
-#define LOGEMERGENCY(text) YGP::Syslog::write (YGP::Syslog::EMERGENCY, text)
-#define LOGALERT(text)     YGP::Syslog::write (YGP::Syslog::ALERT, text)
-#define LOGCRITICAL(text)  YGP::Syslog::write (YGP::Syslog::CRITICAL, text)
-#define LOGERROR(text)     YGP::Syslog::write (YGP::Syslog::ERR, text)
-#define LOGWARNING(text)   YGP::Syslog::write (YGP::Syslog::WARNING, text)
-#define LOGNOTICE(text)    YGP::Syslog::write (YGP::Syslog::NOTICE, text)
-#define LOGINFO(text)      YGP::Syslog::write (YGP::Syslog::INFO, text)
-#define LOGDEBUG(text)     YGP::Syslog::write (YGP::Syslog::DEBUGGING, text)
-
+#define LOGEMERGENCY(text) YGP::Syslog::write(YGP::Syslog::EMERGENCY, text)
+#define LOGALERT(text) YGP::Syslog::write(YGP::Syslog::ALERT, text)
+#define LOGCRITICAL(text) YGP::Syslog::write(YGP::Syslog::CRITICAL, text)
+#define LOGERROR(text) YGP::Syslog::write(YGP::Syslog::ERR, text)
+#define LOGWARNING(text) YGP::Syslog::write(YGP::Syslog::WARNING, text)
+#define LOGNOTICE(text) YGP::Syslog::write(YGP::Syslog::NOTICE, text)
+#define LOGINFO(text) YGP::Syslog::write(YGP::Syslog::INFO, text)
+#define LOGDEBUG(text) YGP::Syslog::write(YGP::Syslog::DEBUGGING, text)
 
 /**Class to perform some logging to either a log-file (if available) or to
    the console.
@@ -51,66 +46,74 @@ namespace YGP {
    Every entry can be classified (according to its serverity).
 */
 class Syslog {
- public:
-   /// Constructor; specifying the logging application
-   Syslog (const char* appl) { Check1 (appl);
+  public:
+    /// Constructor; specifying the logging application
+    Syslog(const char* appl) {
+        Check1(appl);
 #ifdef HAVE_SYSLOG_H
-      openlog (appl, LOG_PID | LOG_CONS, LOG_USER);
+        openlog(appl, LOG_PID | LOG_CONS, LOG_USER);
 #else
-      unsigned int len (strlen (appl) + 1);
-      apAppl[Process::getPID ()] = new char[len];
-      memcpy (apAppl[Process::getPID ()], appl, len);
+        unsigned int len(strlen(appl) + 1);
+        apAppl[Process::getPID()] = new char[len];
+        memcpy(apAppl[Process::getPID()], appl, len);
 #endif // HAVE_SYSLOG_H
-   }
+    }
 
-   /// Constructor; specifying the logging application and the type of that
-   /// application
-   Syslog (const char* appl, int facility) { Check1 (appl);
+    /// Constructor; specifying the logging application and the type of that
+    /// application
+    Syslog(const char* appl, int facility) {
+        Check1(appl);
 #ifdef HAVE_SYSLOG_H
-      openlog (appl, LOG_PID | LOG_CONS, facility);
+        openlog(appl, LOG_PID | LOG_CONS, facility);
 #else
-      unsigned int len (strlen (appl) + 1);
-      apAppl[Process::getPID ()] = new char[len];
-      memcpy (apAppl[Process::getPID ()], appl, len);
+        unsigned int len(strlen(appl) + 1);
+        apAppl[Process::getPID()] = new char[len];
+        memcpy(apAppl[Process::getPID()], appl, len);
 #endif // HAVE_SYSLOG_H
-   }
-   /// Destructor
-   ~Syslog () {
+    }
+    /// Destructor
+    ~Syslog() {
 #ifdef HAVE_SYSLOG_H
-      closelog ();
+        closelog();
 #else
-      delete apAppl[Process::getPID ()];
+        delete apAppl[Process::getPID()];
 #endif
-   }
+    }
 
-   /// Writes a message to the logfile; specifying the level of the message
-   static void write (int level, const char* text) {
-      Check1 (text);
+    /// Writes a message to the logfile; specifying the level of the message
+    static void write(int level, const char* text) {
+        Check1(text);
 #ifdef HAVE_SYSLOG_H
-      syslog (level, "%s", text);
+        syslog(level, "%s", text);
 #else
-// Use printf to log under Windoze (although NT does have some logging-thing)
-      static char* levels[] = { "Emergency", "Alert", "Critical", "Error", "Warning",
-                                "Notice", "Information", "Debug" };
-      Check1 (level < (sizeof (levels) / sizeof (levels[0])));
-      std::cerr << apAppl[Process::getPID ()] << '-' << levels[level] << ": " << text << '\n';
+        // Use printf to log under Windoze (although NT does have some logging-thing)
+        static char* levels[] = {"Emergency", "Alert", "Critical", "Error", "Warning", "Notice", "Information", "Debug"};
+        Check1(level < (sizeof(levels) / sizeof(levels[0])));
+        std::cerr << apAppl[Process::getPID()] << '-' << levels[level] << ": " << text << '\n';
 #endif // HAVE_SYSLOG_H
-   }
+    }
 
 #ifdef HAVE_SYSLOG_H
-   enum { EMERGENCY = LOG_EMERG, ALERT = LOG_ALERT, CRITICAL = LOG_CRIT,
-          ERROR = LOG_ERR, WARNING = LOG_WARNING, NOTICE = LOG_NOTICE,
-          INFO = LOG_INFO, DEBUGGING = LOG_DEBUG };
+    enum {
+        EMERGENCY = LOG_EMERG,
+        ALERT = LOG_ALERT,
+        CRITICAL = LOG_CRIT,
+        ERROR = LOG_ERR,
+        WARNING = LOG_WARNING,
+        NOTICE = LOG_NOTICE,
+        INFO = LOG_INFO,
+        DEBUGGING = LOG_DEBUG
+    };
 #else
-   // Don't rename ERR to ERROR, as this causes an error with BCC
-   enum { EMERGENCY, ALERT, CRITICAL, ERROR, WARNING, NOTICE, INFO, DEBUGGING };
+    // Don't rename ERR to ERROR, as this causes an error with BCC
+    enum { EMERGENCY, ALERT, CRITICAL, ERROR, WARNING, NOTICE, INFO, DEBUGGING };
 
- private:
-   // Declaration is in Version.cpp!
-   static std::map <unsigned int, char*> apAppl;
+  private:
+    // Declaration is in Version.cpp!
+    static std::map<unsigned int, char*> apAppl;
 #endif // HAVE_SYSLOG_H
 };
 
-}
+} // namespace YGP
 
 #endif
